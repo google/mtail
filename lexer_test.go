fd6eb0bb5464f38ff95edc2ea137397185b323e4
Jamie Wilkinson <jaq@spacepants.org>
2012-01-17 17:36:49 +1100

    Rename label to tag to match docs!

--- lexer_test.go
+++ lexer_test.go
@@ -4,114 +4,114 @@
 package main
 
 import (
-        "reflect"
-        "strings"
-        "testing"
+	"reflect"
+	"strings"
+	"testing"
 )
 
 type lexerTest struct {
-        name    string
-        input   string
-        tokens  []Token
+	name   string
+	input  string
+	tokens []Token
 }
 
 var lexerTests = []lexerTest{
-        {"empty", "", []Token{
-                Token{EOF, "", Position{0, 0, 0}}}},
-        {"spaces", " \t\n", []Token{
-                Token{EOF, "", Position{1, 0, 0}}}},
-        {"comment", "# comment", []Token{
-                Token{EOF, "", Position{0, 9, 9}}}},
-        {"comment not at col 1", "  # comment", []Token{
-                Token{EOF, "", Position{0, 11, 11}}}},
-        {"punctuation", "{}(),", []Token{
-                Token{LCURLY, "{", Position{0, 0, 0}},
-                Token{RCURLY, "}", Position{0, 1, 1}},
-                Token{LPAREN, "(", Position{0, 2, 2}},
-                Token{RPAREN, ")", Position{0, 3, 3}},
-                Token{COMMA, ",", Position{0, 4, 4}},
-                Token{EOF, "", Position{0, 5, 5}}}},
-        {"keywords",
-                "inc\nlabel\nstrptime\n", []Token{
-                        Token{BUILTIN, "inc", Position{0, 0, 2}},
-                        Token{BUILTIN, "label", Position{1, 0, 4}},
-                        Token{BUILTIN, "strptime", Position{2, 0, 7}},
-                        Token{EOF, "", Position{3, 0, 0}}}},
-        {"identifer", "a be foo\nquux line-count", []Token{
-                Token{ID, "a", Position{0, 0, 0}},
-                Token{ID, "be", Position{0, 2, 3}},
-                Token{ID, "foo", Position{0, 5, 7}},
-                Token{ID, "quux", Position{1, 0, 3}},
-                Token{ID, "line-count", Position{1, 5, 14}},
-                Token{EOF, "", Position{1, 15, 15}}}},
-        {"regex", "/asdf/", []Token{
-                Token{REGEX, "asdf", Position{0, 0, 5}},
-                Token{EOF, "", Position{0, 6, 6}}}},
-        {"capref", "$foo", []Token{
-                Token{CAPREF, "foo", Position{0, 0, 3}},
-                Token{EOF, "", Position{0, 4, 4}}}},
-        {"numerical capref", "$1", []Token{
-                Token{CAPREF, "1", Position{0, 0, 1}},
-                Token{EOF, "", Position{0, 2, 2}}}},
-        {"capref with trailing punc", "$foo,", []Token{
-                Token{CAPREF, "foo", Position{0, 0, 3}},
-                Token{COMMA, ",", Position{0, 4, 4}},
-                Token{EOF, "", Position{0, 5, 5}}}},
-        {"quoted string", "\"asdf\"", []Token{
-                Token{STRING, "asdf", Position{0, 0, 5}},
-                Token{EOF, "", Position{0, 6, 6}}}},
-        {"escaped slashes in regex", "/foo\\//", []Token{
-                Token{REGEX, "foo\\/", Position{0, 0, 6}},
-                Token{EOF, "", Position{0, 7, 7}}}},
-        {"escaped quote in quoted string", "\"\\\"\"", []Token{
-                Token{STRING, "\\\"", Position{0, 0, 3}},
-                Token{EOF, "", Position{0, 4, 4}}}},
-        {"large program",
-                "/(?P<date>[[:digit:]-\\/ ])/ {\n" +
-                        "  strptime($date, \"%Y/%m/%d %H:%M:%S\")\n" +
-                        "  inc(foo)\n" +
-                        "}", []Token{
-                        Token{REGEX, "(?P<date>[[:digit:]-\\/ ])", Position{0, 0, 26}},
-                        Token{LCURLY, "{", Position{0, 28, 28}},
-                        Token{BUILTIN, "strptime", Position{1, 2, 9}},
-                        Token{LPAREN, "(", Position{1, 10, 10}},
-                        Token{CAPREF, "date", Position{1, 11, 15}},
-                        Token{COMMA, ",", Position{1, 16, 16}},
-                        Token{STRING, "%Y/%m/%d %H:%M:%S", Position{1, 18, 36}},
-                        Token{RPAREN, ")", Position{1, 37, 37}},
-                        Token{BUILTIN, "inc", Position{2, 2, 4}},
-                        Token{LPAREN, "(", Position{2, 5, 5}},
-                        Token{ID, "foo", Position{2, 6, 8}},
-                        Token{RPAREN, ")", Position{2, 9, 9}},
-                        Token{RCURLY, "}", Position{3, 0, 0}},
-                        Token{EOF, "", Position{3, 1, 1}}}},
-        // errors
-        {"unexpected char", "?", []Token{
-                Token{INVALID, "Unexpected input: '?'", Position{0, 0, 0}}}},
-        {"unterminated regex", "/foo\n", []Token{
-                Token{INVALID, "Unterminated regular expression: \"/foo\"", Position{0, 0, 3}}}},
-        {"unterminated quoted string", "\"foo\n", []Token{
-                Token{INVALID, "Unterminated quoted string: \"\\\"foo\"", Position{0, 0, 3}}}},
+	{"empty", "", []Token{
+		Token{EOF, "", Position{0, 0, 0}}}},
+	{"spaces", " \t\n", []Token{
+		Token{EOF, "", Position{1, 0, 0}}}},
+	{"comment", "# comment", []Token{
+		Token{EOF, "", Position{0, 9, 9}}}},
+	{"comment not at col 1", "  # comment", []Token{
+		Token{EOF, "", Position{0, 11, 11}}}},
+	{"punctuation", "{}(),", []Token{
+		Token{LCURLY, "{", Position{0, 0, 0}},
+		Token{RCURLY, "}", Position{0, 1, 1}},
+		Token{LPAREN, "(", Position{0, 2, 2}},
+		Token{RPAREN, ")", Position{0, 3, 3}},
+		Token{COMMA, ",", Position{0, 4, 4}},
+		Token{EOF, "", Position{0, 5, 5}}}},
+	{"keywords",
+		"inc\ntag\nstrptime\n", []Token{
+			Token{BUILTIN, "inc", Position{0, 0, 2}},
+			Token{BUILTIN, "tag", Position{1, 0, 2}},
+			Token{BUILTIN, "strptime", Position{2, 0, 7}},
+			Token{EOF, "", Position{3, 0, 0}}}},
+	{"identifer", "a be foo\nquux line-count", []Token{
+		Token{ID, "a", Position{0, 0, 0}},
+		Token{ID, "be", Position{0, 2, 3}},
+		Token{ID, "foo", Position{0, 5, 7}},
+		Token{ID, "quux", Position{1, 0, 3}},
+		Token{ID, "line-count", Position{1, 5, 14}},
+		Token{EOF, "", Position{1, 15, 15}}}},
+	{"regex", "/asdf/", []Token{
+		Token{REGEX, "asdf", Position{0, 0, 5}},
+		Token{EOF, "", Position{0, 6, 6}}}},
+	{"capref", "$foo", []Token{
+		Token{CAPREF, "foo", Position{0, 0, 3}},
+		Token{EOF, "", Position{0, 4, 4}}}},
+	{"numerical capref", "$1", []Token{
+		Token{CAPREF, "1", Position{0, 0, 1}},
+		Token{EOF, "", Position{0, 2, 2}}}},
+	{"capref with trailing punc", "$foo,", []Token{
+		Token{CAPREF, "foo", Position{0, 0, 3}},
+		Token{COMMA, ",", Position{0, 4, 4}},
+		Token{EOF, "", Position{0, 5, 5}}}},
+	{"quoted string", "\"asdf\"", []Token{
+		Token{STRING, "asdf", Position{0, 0, 5}},
+		Token{EOF, "", Position{0, 6, 6}}}},
+	{"escaped slashes in regex", "/foo\\//", []Token{
+		Token{REGEX, "foo\\/", Position{0, 0, 6}},
+		Token{EOF, "", Position{0, 7, 7}}}},
+	{"escaped quote in quoted string", "\"\\\"\"", []Token{
+		Token{STRING, "\\\"", Position{0, 0, 3}},
+		Token{EOF, "", Position{0, 4, 4}}}},
+	{"large program",
+		"/(?P<date>[[:digit:]-\\/ ])/ {\n" +
+			"  strptime($date, \"%Y/%m/%d %H:%M:%S\")\n" +
+			"  inc(foo)\n" +
+			"}", []Token{
+			Token{REGEX, "(?P<date>[[:digit:]-\\/ ])", Position{0, 0, 26}},
+			Token{LCURLY, "{", Position{0, 28, 28}},
+			Token{BUILTIN, "strptime", Position{1, 2, 9}},
+			Token{LPAREN, "(", Position{1, 10, 10}},
+			Token{CAPREF, "date", Position{1, 11, 15}},
+			Token{COMMA, ",", Position{1, 16, 16}},
+			Token{STRING, "%Y/%m/%d %H:%M:%S", Position{1, 18, 36}},
+			Token{RPAREN, ")", Position{1, 37, 37}},
+			Token{BUILTIN, "inc", Position{2, 2, 4}},
+			Token{LPAREN, "(", Position{2, 5, 5}},
+			Token{ID, "foo", Position{2, 6, 8}},
+			Token{RPAREN, ")", Position{2, 9, 9}},
+			Token{RCURLY, "}", Position{3, 0, 0}},
+			Token{EOF, "", Position{3, 1, 1}}}},
+	// errors
+	{"unexpected char", "?", []Token{
+		Token{INVALID, "Unexpected input: '?'", Position{0, 0, 0}}}},
+	{"unterminated regex", "/foo\n", []Token{
+		Token{INVALID, "Unterminated regular expression: \"/foo\"", Position{0, 0, 3}}}},
+	{"unterminated quoted string", "\"foo\n", []Token{
+		Token{INVALID, "Unterminated quoted string: \"\\\"foo\"", Position{0, 0, 3}}}},
 }
 
 // collect gathers the emitted items into a slice.
 func collect(t *lexerTest) (tokens []Token) {
-        l := NewLexer(t.name, strings.NewReader(t.input))
-        for {
-                token := l.NextToken()
-                tokens = append(tokens, token)
-                if token.kind == EOF || token.kind == INVALID {
-                        break
-                }
-        }
-        return
+	l := NewLexer(t.name, strings.NewReader(t.input))
+	for {
+		token := l.NextToken()
+		tokens = append(tokens, token)
+		if token.kind == EOF || token.kind == INVALID {
+			break
+		}
+	}
+	return
 }
 
 func TestLex(t *testing.T) {
-        for _, test := range lexerTests {
-                tokens := collect(&test)
-                if !reflect.DeepEqual(tokens, test.tokens) {
-                        t.Errorf("%s: got\n\t%v\nexpected\n\t%v", test.name, tokens, test.tokens)
-                }
-        }
+	for _, test := range lexerTests {
+		tokens := collect(&test)
+		if !reflect.DeepEqual(tokens, test.tokens) {
+			t.Errorf("%s: got\n\t%v\nexpected\n\t%v", test.name, tokens, test.tokens)
+		}
+	}
 }
--- vm.go
+++ vm.go
@@ -4,246 +4,246 @@
 package main
 
 import (
-        "fmt"
-        "io"
-        "log"
-        "regexp"
-        "strconv"
-        "time"
+	"fmt"
+	"io"
+	"log"
+	"regexp"
+	"strconv"
+	"time"
 )
 
 type opcode int
 
 const (
-        match   opcode  = iota
-        jnm
-        inc
-        strptime
-        label
-        ret
-        push
-        capref
-        load
+	match opcode = iota
+	jnm
+	inc
+	strptime
+	tag
+	ret
+	push
+	capref
+	load
 )
 
 var opNames = map[opcode]string{
-        match:    "match",
-        jnm:      "jnm",
-        inc:      "inc",
-        strptime: "strptime",
-        label:    "label",
-        ret:      "ret",
-        push:     "push",
-        capref:   "capref",
-        load:     "load",
+	match:    "match",
+	jnm:      "jnm",
+	inc:      "inc",
+	strptime: "strptime",
+	tag:      "tag",
+	ret:      "ret",
+	push:     "push",
+	capref:   "capref",
+	load:     "load",
 }
 
 var builtin = map[string]opcode{
-        "inc":      inc,
-        "strptime": strptime,
-        "label":    label,
+	"inc":      inc,
+	"strptime": strptime,
+	"tag":      tag,
 }
 
 type instr struct {
-        op      opcode
-        opnd    int
+	op   opcode
+	opnd int
 }
 
 func (i instr) String() string {
-        return fmt.Sprintf("%s %d", opNames[i.op], i.opnd)
+	return fmt.Sprintf("%s %d", opNames[i.op], i.opnd)
 }
 
 type thread struct {
-        pc      int
-        reg     int
-        matches []string
-        time    time.Time
-        labels  map[string]string
+	pc      int
+	reg     int
+	matches []string
+	time    time.Time
+	tags    map[string]string
 }
 
 type vm struct {
-        prog    []instr
-        re      []*regexp.Regexp
+	prog []instr
+	re   []*regexp.Regexp
 
-        str     []string
+	str []string
 
-        stack   []interface{}
+	stack []interface{}
 }
 
 func (v *vm) pop() interface{} {
-        m := v.stack[len(v.stack)-1]
-        v.stack = v.stack[:len(v.stack)-1]
-        return m
+	m := v.stack[len(v.stack)-1]
+	v.stack = v.stack[:len(v.stack)-1]
+	return m
 }
 
 func (v *vm) push(opnd interface{}) {
-        v.stack = append(v.stack, opnd)
+	v.stack = append(v.stack, opnd)
 }
 
 func (v *vm) Run(input string) bool {
-        // Create a first thread to execute.
-        t := new(thread)
-        for {
-                // fetch
-                if t.pc >= len(v.prog) {
-                        return false
-                }
-                i := v.prog[t.pc]
+	// Create a first thread to execute.
+	t := new(thread)
+	for {
+		// fetch
+		if t.pc >= len(v.prog) {
+			return false
+		}
+		i := v.prog[t.pc]
 
-                // execute
-                switch i.op {
-                case match:
-                        t.labels = map[string]string{}
-                        // match regex and stre success
-                        t.matches = v.re[i.opnd].FindStringSubmatch(input)
-                        if t.matches != nil {
-                                t.reg = 1
-                        } else {
-                                t.reg = 0
-                        }
-                case jnm:
-                        if t.reg == 0 {
-                                t.pc = i.opnd
-                                continue
-                        }
-                case inc:
-                        // increment a counter
-                        m := v.pop().(int)
-                        metrics[m].value++
-                        metrics[m].time = t.time
-                case strptime:
-                        layout := v.pop().(string)
-                        s := v.pop().(string)
-                        tm, err := time.Parse(layout, s)
-                        if err != nil {
-                                log.Println("time parse:", err)
-                                break
-                        }
-                        t.time = tm
-                case label:
-                        val := v.pop().(string)
-                        key := v.pop().(string)
-                        t.labels[key] = val
-                case capref:
-                        v.push(t.matches[i.opnd])
-                case load:
-                        v.push(v.str[i.opnd])
-                case ret:
-                        // reg is an int 1 or 0 for true/false
-                        return i.opnd == 1
-                case push:
-                        v.push(i.opnd)
-                default:
-                        log.Println("illegal instruction", i.op)
-                        return false
-                }
-                t.pc++
-        }
-        panic("not reached")
+		// execute
+		switch i.op {
+		case match:
+			t.tags = map[string]string{}
+			// match regex and stre success
+			t.matches = v.re[i.opnd].FindStringSubmatch(input)
+			if t.matches != nil {
+				t.reg = 1
+			} else {
+				t.reg = 0
+			}
+		case jnm:
+			if t.reg == 0 {
+				t.pc = i.opnd
+				continue
+			}
+		case inc:
+			// increment a counter
+			m := v.pop().(int)
+			metrics[m].value++
+			metrics[m].time = t.time
+		case strptime:
+			layout := v.pop().(string)
+			s := v.pop().(string)
+			tm, err := time.Parse(layout, s)
+			if err != nil {
+				log.Println("time parse:", err)
+				break
+			}
+			t.time = tm
+		case tag:
+			val := v.pop().(string)
+			key := v.pop().(string)
+			t.tags[key] = val
+		case capref:
+			v.push(t.matches[i.opnd])
+		case load:
+			v.push(v.str[i.opnd])
+		case ret:
+			// reg is an int 1 or 0 for true/false
+			return i.opnd == 1
+		case push:
+			v.push(i.opnd)
+		default:
+			log.Println("illegal instruction", i.op)
+			return false
+		}
+		t.pc++
+	}
+	panic("not reached")
 }
 
 type compiler struct {
-        prog    []instr
-        str     []string
-        re      []*regexp.Regexp
+	prog []instr
+	str  []string
+	re   []*regexp.Regexp
 
-        metrics map[string]int
-        refs    []int
+	metrics map[string]int
+	refs    []int
 }
 
 func Compile(name string, input io.Reader) (*vm, []string) {
-        p := NewParser(name, input)
-        EmtailParse(p)
-        if p == nil || len(p.errors) > 0 {
-                return nil, p.errors
-        }
-        c := &compiler{}
-        p.root.acceptVisitor(c)
-        vm := &vm{
-                re:     c.re,
-                str:    c.str,
-                prog:   c.prog}
-        return vm, nil
+	p := NewParser(name, input)
+	EmtailParse(p)
+	if p == nil || len(p.errors) > 0 {
+		return nil, p.errors
+	}
+	c := &compiler{}
+	p.root.acceptVisitor(c)
+	vm := &vm{
+		re:   c.re,
+		str:  c.str,
+		prog: c.prog}
+	return vm, nil
 }
 
 func (c *compiler) emit(i instr) {
-        c.prog = append(c.prog, i)
+	c.prog = append(c.prog, i)
 }
 
 func (c *compiler) visitStmtList(n stmtlistNode) {
-        for _, child := range n.children {
-                child.acceptVisitor(c)
-        }
+	for _, child := range n.children {
+		child.acceptVisitor(c)
+	}
 }
 
 func (c *compiler) visitExprList(n exprlistNode) {
-        for _, child := range n.children {
-                child.acceptVisitor(c)
-        }
+	for _, child := range n.children {
+		child.acceptVisitor(c)
+	}
 }
 
 func (c *compiler) visitCond(n condNode) {
-        // TODO(jaq): split
-        if n.cond != nil {
-                n.cond.acceptVisitor(c)
-        }
-        c.emit(instr{op: jnm})
-        // Save PC
-        pc := len(c.prog) - 1
-        for _, child := range n.children {
-                child.acceptVisitor(c)
-        }
-        c.emit(instr{ret, 1})
-        // rewrite jump label
-        c.prog[pc].opnd = len(c.prog)
+	// TODO(jaq): split
+	if n.cond != nil {
+		n.cond.acceptVisitor(c)
+	}
+	c.emit(instr{op: jnm})
+	// Save PC of jump instruction
+	pc := len(c.prog) - 1
+	for _, child := range n.children {
+		child.acceptVisitor(c)
+	}
+	c.emit(instr{ret, 1})
+	// rewrite jump target
+	c.prog[pc].opnd = len(c.prog)
 }
 
 func (c *compiler) visitRegex(n regexNode) {
-        re, err := regexp.Compile(n.pattern)
-        if err != nil {
-                log.Fatal("regex:", err)
-        } else {
-                c.re = append(c.re, re)
-                c.emit(instr{match, len(c.re) - 1})
-        }
+	re, err := regexp.Compile(n.pattern)
+	if err != nil {
+		log.Fatal("regex:", err)
+	} else {
+		c.re = append(c.re, re)
+		c.emit(instr{match, len(c.re) - 1})
+	}
 }
 
 func (c *compiler) visitString(n stringNode) {
-        c.str = append(c.str, n.text)
-        c.emit(instr{load, len(c.str) - 1})
+	c.str = append(c.str, n.text)
+	c.emit(instr{load, len(c.str) - 1})
 }
 
 func (c *compiler) visitId(n idNode) {
-        i, ok := c.metrics[n.name]
-        if !ok {
-                m := &metric{name: n.name, typ: counter}
-                metrics = append(metrics, m)
-                c.emit(instr{push, len(metrics) - 1})
-        } else {
-                c.emit(instr{push, i})
-        }
+	i, ok := c.metrics[n.name]
+	if !ok {
+		m := &metric{name: n.name, typ: counter}
+		metrics = append(metrics, m)
+		c.emit(instr{push, len(metrics) - 1})
+	} else {
+		c.emit(instr{push, i})
+	}
 }
 
 func (c *compiler) visitCapref(n caprefNode) {
-        i, err := strconv.Atoi(n.name)
-        if err != nil {
-                log.Printf("capref: %s", err)
-        }
-        c.emit(instr{capref, i})
+	i, err := strconv.Atoi(n.name)
+	if err != nil {
+		log.Printf("capref: %s", err)
+	}
+	c.emit(instr{capref, i})
 }
 
 func (c *compiler) visitBuiltin(n builtinNode) {
-        for _, child := range n.children {
-                child.acceptVisitor(c)
-        }
-        c.emit(instr{op: builtin[n.name]})
+	for _, child := range n.children {
+		child.acceptVisitor(c)
+	}
+	c.emit(instr{op: builtin[n.name]})
 }
 
 func (v *vm) Start(lines chan string) {
-        for {
-                select {
-                case line := <-lines:
-                        v.Run(line)
-                }
-        }
+	for {
+		select {
+		case line := <-lines:
+			v.Run(line)
+		}
+	}
 }
