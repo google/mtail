fc350a64067822d22d0396bc96a13dae11f34437
Jamie Wilkinson <jaq@google.com>
2012-08-22 13:39:17 +1000

    Update emtail-mode.

--- emtail-mode.el
+++ emtail-mode.el
@@ -1,6 +1,8 @@
 ; Copyright 2011 Google Inc. All Rights Reserved.
 ; This file is available under the Apache license.
 
+(defvar emtail-mode-hook nil)
+
 (defcustom emtail-indent-offset 2
   "Indent offset for `emtail-mode'.")
 
@@ -24,34 +26,38 @@
     st)
   "Syntax table used while in `emtail-mode'.")
 
+(defvar emtail-mode-types
+  '("counter" "gauge")
+  "All types in the emtail language.  Used for font locking.")
+
 (defvar emtail-mode-keywords
-  '("counter" "gauge" "as" "by" "hidden")
+  '("as" "by" "hidden" "if")
   "All keywords in the emtail language.  Used for font locking.")
 
 (defvar emtail-mode-builtins
   '("strptime" "timestamp")
-  "All keywords in the emtail language.  Used for font locking.")
+  "All builtins in the emtail language.  Used for font locking.")
 
 (defvar emtail-mode-font-lock-defaults
-      `((
-         ("\\<\\(FIXME\\|TODO\\)" 1 font-lock-warning-face prepend)
-         (,(regexp-opt emtail-mode-keywords 'words) . font-lock-keyword-face)
-         (,(regexp-opt emtail-mode-builtins 'words) . font-lock-builtin-face)
-         ("\\<\\$?[a-zA-Z0-9_]+\\>" . font-lock-variable-name-face)
-         )))
+  `((
+     (,(regexp-opt emtail-mode-types 'words) . font-lock-type-face)
+     (,(regexp-opt emtail-mode-builtins 'words) . font-lock-builtin-face)
+     ("\\<\\$?[a-zA-Z0-9_]+\\>" . font-lock-variable-name-face)
+     (,(regexp-opt emtail-mode-keywords 'words) . font-lock-keyword-face)
+     )))
 
+;;;###autoload
 (define-derived-mode emtail-mode nil "emtail"
   "Major mode for editing emtail programs."
-  ;; Font lock
-  (set (make-local-variable 'font-lock-defaults)
-       '(emtail-mode-font-lock-defaults nil nil nil nil))
-
+  :syntax-table emtail-mode-syntax-table
   (set (make-local-variable 'paragraph-separate) "^[ \t]*$")
   (set (make-local-variable 'paragraph-start) "^[ \t]*$")
   (set (make-local-variable 'comment-start) "# ")
   (set (make-local-variable 'comment-end) "")
   (set (make-local-variable 'comment-start-skip) "# *")
-
+  ;; Font lock
+  (set (make-local-variable 'font-lock-defaults)
+       '(emtail-mode-font-lock-defaults))
   (setq indent-tabs-mode nil))
 
 
@@ -61,6 +67,7 @@
   "Reload emtail-mode.el and put the current buffer into emtail-mode.  Useful for debugging."
   (interactive)
   (unload-feature 'emtail-mode)
+  (add-to-list 'load-path "/home/jaq/src/emtail")
   (require 'emtail-mode)
   (emtail-mode))
 
