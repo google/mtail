b5646b5334f42563191a2a35bf328ffbe3dac16f
Jamie Wilkinson <jaq@spacepants.org>
2015-02-05 21:09:45 +1100

    Add support for exporting in the Prometheus text format.

---
+++ export_prometheus.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"expvar"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+var (
+	metric_export_total   = expvar.NewInt("metric_export_total")
+	metric_export_success = expvar.NewInt("metric_exprot_success")
+)
+
+const (
+	PROMETHEUS_FORMAT = "%s {%s} %d %d\n"
+)
+
+func NoHyphens(s string) string {
+	return strings.Replace(s, "-", "_", -1)
+}
+
+func handlePrometheusMetrics(w http.ResponseWriter, r *http.Request) {
+	metric_lock.RLock()
+	defer metric_lock.RUnlock()
+
+	w.Header().Add("Content-type", "text/plain; version=0.0.4")
+
+	lc := make(chan *LabelSet)
+	quit := make(chan bool, 1)
+	for _, m := range metrics {
+		metric_export_total.Add(1)
+		fmt.Fprintf(w,
+			"# TYPE %s %s\n",
+			NoHyphens(m.Name),
+			strings.ToLower(m.Kind.String()))
+		go m.EmitLabelSets(lc, quit)
+		for {
+			select {
+			case l := <-lc:
+				line := MetricToPrometheus(m, l)
+				fmt.Fprintf(w, line)
+			case <-quit:
+				goto ret
+			}
+		}
+	ret:
+	}
+}
+
+func MetricToPrometheus(m *Metric, l *LabelSet) string {
+	var s []string
+	for k, v := range l.labels {
+		s = append(s, fmt.Sprintf("%s=\"%s\"", k, v))
+	}
+	s = append(s, fmt.Sprintf("prog=\"%s\"", m.Program))
+	s = append(s, fmt.Sprintf("instance=\"%s\"", hostname))
+	return fmt.Sprintf(PROMETHEUS_FORMAT,
+		NoHyphens(m.Name),
+		strings.Join(s, ","),
+		l.datum.Get(),
+		l.datum.Time.Unix())
+}
