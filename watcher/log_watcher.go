d9817a7514d533e85038a5b0c070345fb33af04c
Jamie Wilkinson <jaq@spacepants.org>
2015-04-02 16:11:25 +1100

    Convert the log watcher to use range over the events channel and remove the quit channel.  Handle the Errors channel in a different goroutine.

--- watcher/log_watcher.go
+++ watcher/log_watcher.go
@@ -14,7 +14,6 @@ var (
 type LogWatcher struct {
 	*fsnotify.Watcher
 	events chan Event
-	quit   chan struct{}
 }
 
 func NewLogWatcher() (w *LogWatcher, err error) {
@@ -22,7 +21,7 @@ func NewLogWatcher() (w *LogWatcher, err error) {
 	if err != nil {
 		return
 	}
-	w = &LogWatcher{f, make(chan Event), make(chan struct{})}
+	w = &LogWatcher{f, make(chan Event)}
 	go w.run()
 	return
 }
@@ -30,35 +29,26 @@ func NewLogWatcher() (w *LogWatcher, err error) {
 func (w *LogWatcher) Events() <-chan Event { return w.events }
 
 func (w *LogWatcher) run() {
-	for {
-		select {
-		case e, more := <-w.Watcher.Events:
-			event_count.Add(e.Name, 1)
-			glog.Infof("Writing %v", e)
-			switch {
-			case e.Op&fsnotify.Create == fsnotify.Create:
-				w.events <- CreateEvent{e.Name}
-			case e.Op&fsnotify.Write == fsnotify.Write:
-				w.events <- UpdateEvent{e.Name}
-			case e.Op&fsnotify.Remove == fsnotify.Remove:
-				w.events <- DeleteEvent{e.Name}
-			default:
-				glog.Infof("Unexpected event type detected: %q", e)
-			}
-			if !more {
-				goto end
-			}
-		case err := <-w.Errors:
+	// Suck out errors and dump them to the error log.
+	go func() {
+		for err := range w.Watcher.Errors {
 			glog.Errorf("fsnotify error: %s\n", err)
-		case <-w.quit:
-			goto end
+		}
+	}()
+	for e := range w.Watcher.Events {
+		event_count.Add(e.Name, 1)
+		glog.Infof("Writing %v", e)
+		switch {
+		case e.Op&fsnotify.Create == fsnotify.Create:
+			w.events <- CreateEvent{e.Name}
+		case e.Op&fsnotify.Write == fsnotify.Write:
+			w.events <- UpdateEvent{e.Name}
+		case e.Op&fsnotify.Remove == fsnotify.Remove:
+			w.events <- DeleteEvent{e.Name}
+		default:
+			glog.Infof("Unexpected event type detected: %q", e)
 		}
 	}
-end:
+	glog.Infof("Shutting down log watcher.")
 	close(w.events)
-}
-
-func (w *LogWatcher) Close() error {
-	close(w.quit)
-	return w.Watcher.Close()
 }
