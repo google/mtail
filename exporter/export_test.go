5dba799b5941484b3d71e20df7cbe0da121c8acb
Jamie Wilkinson <jaq@spacepants.org>
2015-04-17 12:28:16 +1000

    Add a test to parse the JSON output of the metrics store from the HandleJSON exporter method.

--- exporter/export_test.go
+++ exporter/export_test.go
@@ -4,6 +4,9 @@
 package exporter
 
 import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"reflect"
 	"sort"
@@ -117,5 +120,65 @@ func TestMetricToStatsd(t *testing.T) {
 		"prog.bar.l.snuh:42|c"}
 	if !reflect.DeepEqual(expected, r) {
 		t.Errorf("String didn't match:\n\texpected: %v\n\treceived: %v", expected, r)
+	}
+}
+
+var handleJSONTests = []struct {
+	name     string
+	metrics  []metrics.Metric
+	expected string
+}{
+	{"empty",
+		[]metrics.Metric{},
+		"null",
+	},
+	{"single",
+		[]metrics.Metric{
+			metrics.Metric{
+				Name:        "foo",
+				Program:     "test",
+				Kind:        metrics.Counter,
+				LabelValues: []*metrics.LabelValue{&metrics.LabelValue{[]string{}, &metrics.Datum{Value: 1}}},
+			},
+		},
+		`[
+  {
+    "Name": "foo",
+    "Program": "test",
+    "Kind": 0,
+    "LabelValues": [
+      {
+        "Value": {
+          "Value": 1,
+          "Time": "0001-01-01T00:00:00Z"
+        }
+      }
+    ]
+  }
+]`,
+	},
+}
+
+func TestHandleJSON(t *testing.T) {
+	for _, tc := range handleJSONTests {
+
+		ms := metrics.Store{}
+		for _, metric := range tc.metrics {
+			ms.Add(&metric)
+		}
+		e := New(&ms)
+		response := httptest.NewRecorder()
+		e.HandleJSON(response, &http.Request{})
+		if response.Code != 200 {
+			t.Errorf("test case %s: response code not 200: %s", tc.name, response.Code)
+		}
+		b, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			t.Errorf("test case %s: failed to read response: %s", tc.name, err)
+		}
+		diff := pretty.Compare(string(b), tc.expected)
+		if len(diff) > 0 {
+			t.Errorf("test case %s: response not expected:\n%s", tc.name, diff)
+		}
 	}
 }
