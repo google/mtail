e1ce10dfd069f1e2888747c9af1cf6afee49ebfc
Jamie Wilkinson <jaq@spacepants.org>
2015-09-01 09:30:25 +1000

    Hide info logs, and clean up ex_test to use new reader code exclusively.

--- ex_test.go
+++ ex_test.go
@@ -30,7 +30,6 @@ var exampleProgramTests = []struct {
 		"examples/rsyncd.mtail",
 		"testdata/rsyncd.log",
 		"testdata/rsyncd.golden",
-		//		"testdata/rsyncd.json",
 	},
 	// {
 	// 	"examples/sftp.mtail",
@@ -72,44 +71,19 @@ func TestExamplePrograms(t *testing.T) {
 			continue
 		}
 
-		// // Dirty hack to create json files :)
-		// if false {
-		// 	j, err := os.Create(tc.jsonfile)
-		// 	if err != nil {
-		// 		t.Errorf("%s: could not open json file: %s", tc.jsonfile, err)
-		// 		continue
-		// 	}
-		// 	if err := mtail.WriteMetrics(j); err != nil {
-		// 		t.Errorf("couldn't marshall metrics: %q", err)
-		// 		continue
-		// 	}
-		// 	j.WriteString("\n")
-		// 	j.Close()
-		// }
-
 		j, err := os.Open(tc.jsonfile)
 		if err != nil {
 			t.Fatalf("%s: could not open json file: %s", tc.jsonfile, err)
 		}
 		defer j.Close()
-		// var ex bytes.Buffer
 
-		// if _, err := ex.ReadFrom(j); err != nil {
-		// 	t.Fatalf("Couldn't read from json: %s", err)
-		// }
 		golden_store := &metrics.Store{}
 		testdata.ReadTestData(j, tc.programfile, golden_store)
 
 		mtail.Close()
-		// var m bytes.Buffer
-		// mtail.WriteMetrics(&m)
-		// m.WriteString("\n") // Golden data has trailing newline.
 
 		diff := pretty.Compare(golden_store, store)
 
-		// mtail.
-		// strings.Split(ex.String(), "\n"), // want
-		// strings.Split(m.String(), "\n"))  // got
 		if len(diff) > 0 {
 			t.Errorf("%s: metrics don't match:\n%s", tc.programfile, diff)
 
--- testdata/reader.go
+++ testdata/reader.go
@@ -2,7 +2,6 @@ package testdata
 
 import (
 	"bufio"
-	"fmt"
 	"io"
 	"path/filepath"
 	"regexp"
@@ -32,27 +31,27 @@ func ReadTestData(file io.Reader, programfile string, store *metrics.Store) {
 	prog := filepath.Base(programfile)
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		fmt.Printf("'%s'\n", scanner.Text())
+		glog.V(2).Infof("'%s'\n", scanner.Text())
 		match := var_re.FindStringSubmatch(scanner.Text())
-		fmt.Printf("len match: %d\n", len(match))
+		glog.V(2).Infof("len match: %d\n", len(match))
 		if len(match) == 0 {
 			continue
 		}
 		var keys, vals []string
 		if match[3] != "" {
 			for _, pair := range strings.Split(match[4], ",") {
-				fmt.Printf("pair: %s\n", pair)
+				glog.V(2).Infof("pair: %s\n", pair)
 				kv := strings.Split(pair, "=")
 				keys = append(keys, kv[0])
 				vals = append(vals, kv[1])
 			}
 		}
 		var timestamp time.Time
-		fmt.Printf("match 7: %q\n", match[7])
+		glog.V(2).Infof("match 7: %q\n", match[7])
 		if match[7] != "" {
 			timestamp, _ = time.Parse(time.RFC3339, match[7])
 		}
-		fmt.Printf("timestamp is %s which is %v in unix\n", timestamp.Format(time.RFC3339), timestamp.Unix())
+		glog.V(2).Infof("timestamp is %s which is %v in unix\n", timestamp.Format(time.RFC3339), timestamp.Unix())
 
 		val, err := strconv.ParseInt(match[5], 10, 64)
 		if err != nil {
@@ -70,17 +69,17 @@ func ReadTestData(file io.Reader, programfile string, store *metrics.Store) {
 				kind = metrics.Timer
 			}
 			m = metrics.NewMetric(match[2], prog, kind, keys...)
-			fmt.Printf("making a new %v\n", m)
+			glog.V(2).Infof("making a new %v\n", m)
 			store.Add(m)
 		} else {
-			fmt.Printf("found %v\n", m)
+			glog.V(2).Infof("found %v\n", m)
 		}
 		d, err := m.GetDatum(vals...)
 		if err != nil {
-			fmt.Printf("Failed to get datum: %s\n", err)
+			glog.V(2).Infof("Failed to get datum: %s\n", err)
 			continue
 		}
-		fmt.Printf("setting %v with vals %v to %v\n", d, vals, val)
+		glog.V(2).Infof("setting %v with vals %v to %v\n", d, vals, val)
 		d.Set(val, timestamp)
 	}
 }
