20bb12dd503d6174e6e14b7a503571fd91411638
Jamie Wilkinson <jaq@spacepants.org>
2016-01-17 11:56:43 +1100

    Convert sleeps into polls with timeout.

--- mtail/mtail_test.go
+++ mtail/mtail_test.go
@@ -4,6 +4,7 @@
 package mtail
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -53,6 +54,24 @@ func startMtail(t *testing.T, logPathnames []string, progPathname string) *Mtail
 	return m
 }
 
+func doOrTimeout(do func() (bool, error), deadline, interval time.Duration) (bool, error) {
+	timeout := time.After(deadline)
+	ticker := time.Tick(interval)
+	for {
+		select {
+		case <-timeout:
+			return false, errors.New("timeout")
+		case <-ticker:
+			ok, err := do()
+			if err != nil {
+				return false, err
+			} else if ok {
+				return true, nil
+			}
+		}
+	}
+}
+
 func TestHandleLogUpdates(t *testing.T) {
 	if testing.Short() {
 		t.Skip("skipping test in short mode")
@@ -73,11 +92,19 @@ func TestHandleLogUpdates(t *testing.T) {
 	for i, x := range inputLines {
 		// write to log file
 		logFile.WriteString(x + "\n")
-		// TODO(jaq): remove slow sleep
-		time.Sleep(100 * time.Millisecond)
 		// check log line count increase
 		expected := fmt.Sprintf("%d", i+1)
-		if vm.LineCount.String() != expected {
+		check := func() (bool, error) {
+			if vm.LineCount.String() != expected {
+				return false, nil
+			}
+			return true, nil
+		}
+		ok, err := doOrTimeout(check, 100*time.Millisecond, 10*time.Millisecond)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !ok {
 			t.Errorf("Line count not increased\n\texpected: %s\n\treceived: %s", expected, vm.LineCount.String())
 			buf := make([]byte, 1<<16)
 			count := runtime.Stack(buf, true)
@@ -176,11 +203,19 @@ func TestHandleNewLogAfterStart(t *testing.T) {
 		logFile.WriteString(x + "\n")
 		logFile.Sync()
 	}
-	// TODO(jaq): remove slow sleep
-	time.Sleep(100 * time.Millisecond)
 	// check log line count increase
 	expected := fmt.Sprintf("%d", len(inputLines))
-	if vm.LineCount.String() != expected {
+	check := func() (bool, error) {
+		if vm.LineCount.String() != expected {
+			return false, nil
+		}
+		return true, nil
+	}
+	ok, err := doOrTimeout(check, 100*time.Millisecond, 10*time.Millisecond)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !ok {
 		t.Errorf("Line count not increased\n\texpected: %s\n\treceived: %s", expected, vm.LineCount.String())
 	}
 }
