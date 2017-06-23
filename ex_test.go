27a0c75a7bec540baaf2c985a8b75bcad3b65396
Jamie Wilkinson <jaq@spacepants.org>
2017-06-24 06:04:30 +1000

    Better formatting of golden store vs regular store.

--- ex_test.go
+++ ex_test.go
@@ -96,8 +96,8 @@ func TestExamplePrograms(t *testing.T) {
 
 		if diff != nil {
 			t.Errorf("%s: metrics don't match:\n%v", tc.programfile, diff)
-			t.Errorf("Golden metrics: %#v", golden_store.Metrics)
-			t.Errorf("Program metrics: %#v", store.Metrics)
+			t.Errorf(" Golden metrics: %s", golden_store.Metrics)
+			t.Errorf("Program metrics: %s", store.Metrics)
 		}
 	}
 }
