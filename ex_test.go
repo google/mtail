67fae9152615c4d31b10a35783dd386b9f59ce3d
Jamie Wilkinson <jaq@spacepants.org>
2017-06-26 15:34:14 +0200

    Test xntp_peerstats and uncomment setting of peer_offset, peer_dispersion, and peer_delay.  Fix golden test data for correct values.

--- ex_test.go
+++ ex_test.go
@@ -34,16 +34,16 @@ var exampleProgramTests = []struct {
 	// 	"testdata/anonymised_dhcpd_log",
 	// 	"testdata/anonymised_dhcpd_log.golden",
 	// },
-	{
-		"examples/ntpd.mtail",
-		"testdata/ntp4",
-		"testdata/ntp4.golden",
-	},
 	// {
 	// 	"examples/ntpd.mtail",
-	// 	"testdata/xntp3_peerstats",
-	// 	"testdata/xntp3_peerstats.golden",
+	// 	"testdata/ntp4",
+	// 	"testdata/ntp4.golden",
 	// },
+	{
+		"examples/ntpd.mtail",
+		"testdata/xntp3_peerstats",
+		"testdata/xntp3_peerstats.golden",
+	},
 	// {
 	// 	"examples/otherwise.mtail",
 	// 	"testdata/otherwise.log",
