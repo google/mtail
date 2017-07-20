ab52bf0b924b5c5b8e30c2dd11eb48f0378fd77a
Jamie Wilkinson <jaq@spacepants.org>
2017-07-21 03:50:52 +1000

    Fixes the reader test to suite the format of a metric store.

--- testdata/reader_test.go
+++ testdata/reader_test.go
@@ -10,74 +10,89 @@ import (
 	"github.com/google/mtail/metrics/datum"
 )
 
-var expectedMetrics = []*metrics.Metric{
-	&metrics.Metric{
-		Name:    "bytes_total",
-		Program: "reader_test",
-		Kind:    metrics.Counter,
-		Keys:    []string{"operation"},
-		LabelValues: []*metrics.LabelValue{
-			&metrics.LabelValue{
-				Labels: []string{"sent"},
-				Value:  datum.MakeInt(62793673, time.Date(2011, 2, 23, 5, 54, 10, 0, time.UTC))},
-			&metrics.LabelValue{
-				Labels: []string{"received"},
-				Value:  datum.MakeInt(975017, time.Date(2011, 2, 23, 5, 54, 10, 0, time.UTC))}}},
-	&metrics.Metric{
-		Name:    "connections_total",
-		Program: "reader_test",
-		Kind:    metrics.Counter,
-		Keys:    []string{},
-		LabelValues: []*metrics.LabelValue{
-			&metrics.LabelValue{
-				Value: datum.MakeInt(52, time.Date(2011, 2, 22, 21, 54, 13, 0, time.UTC))}}},
-	&metrics.Metric{
-		Name:    "connection-time_total",
-		Program: "reader_test",
-		Kind:    metrics.Counter,
-		Keys:    []string{},
-		LabelValues: []*metrics.LabelValue{
-			&metrics.LabelValue{
-				Value: datum.MakeInt(1181011, time.Date(2011, 2, 23, 5, 54, 10, 0, time.UTC))}}},
-	&metrics.Metric{
-		Name:    "transfers_total",
-		Program: "reader_test",
-		Kind:    metrics.Counter,
-		Keys:    []string{"operation", "module"},
-		LabelValues: []*metrics.LabelValue{
-			&metrics.LabelValue{
-				Labels: []string{"send", "module"},
-				Value:  datum.MakeInt(2, time.Date(2011, 2, 23, 5, 50, 32, 0, time.UTC))},
-			&metrics.LabelValue{
-				Labels: []string{"send", "repo"},
-				Value:  datum.MakeInt(25, time.Date(2011, 2, 23, 5, 51, 14, 0, time.UTC))}}},
-	&metrics.Metric{
-		Name:        "foo",
-		Program:     "reader_test",
-		Kind:        metrics.Gauge,
-		Keys:        []string{"label"},
-		LabelValues: []*metrics.LabelValue{},
+var expectedMetrics = map[string][]*metrics.Metric{
+	"bytes_total": {
+		&metrics.Metric{
+			Name:    "bytes_total",
+			Program: "reader_test",
+			Kind:    metrics.Counter,
+			Keys:    []string{"operation"},
+			LabelValues: []*metrics.LabelValue{
+				&metrics.LabelValue{
+					Labels: []string{"sent"},
+					Value:  datum.MakeInt(62793673, time.Date(2011, 2, 23, 5, 54, 10, 0, time.UTC))},
+				&metrics.LabelValue{
+					Labels: []string{"received"},
+					Value:  datum.MakeInt(975017, time.Date(2011, 2, 23, 5, 54, 10, 0, time.UTC))}}},
 	},
-	&metrics.Metric{
-		Name:    "bar",
-		Program: "reader_test",
-		Kind:    metrics.Counter,
-		Keys:    []string{},
-		LabelValues: []*metrics.LabelValue{
-			&metrics.LabelValue{
-				Value: datum.MakeInt(0, time.Unix(0, 0)),
+	"connections_total": {
+		&metrics.Metric{
+			Name:    "connections_total",
+			Program: "reader_test",
+			Kind:    metrics.Counter,
+			Keys:    []string{},
+			LabelValues: []*metrics.LabelValue{
+				&metrics.LabelValue{
+					Value: datum.MakeInt(52, time.Date(2011, 2, 22, 21, 54, 13, 0, time.UTC))}}},
+	},
+	"connection-time_total": {
+		&metrics.Metric{
+			Name:    "connection-time_total",
+			Program: "reader_test",
+			Kind:    metrics.Counter,
+			Keys:    []string{},
+			LabelValues: []*metrics.LabelValue{
+				&metrics.LabelValue{
+					Value: datum.MakeInt(1181011, time.Date(2011, 2, 23, 5, 54, 10, 0, time.UTC))}}},
+	},
+	"transfers_total": {
+		&metrics.Metric{
+			Name:    "transfers_total",
+			Program: "reader_test",
+			Kind:    metrics.Counter,
+			Keys:    []string{"operation", "module"},
+			LabelValues: []*metrics.LabelValue{
+				&metrics.LabelValue{
+					Labels: []string{"send", "module"},
+					Value:  datum.MakeInt(2, time.Date(2011, 2, 23, 5, 50, 32, 0, time.UTC))},
+				&metrics.LabelValue{
+					Labels: []string{"send", "repo"},
+					Value:  datum.MakeInt(25, time.Date(2011, 2, 23, 5, 51, 14, 0, time.UTC))}}},
+	},
+	"foo": {
+		&metrics.Metric{
+			Name:        "foo",
+			Program:     "reader_test",
+			Kind:        metrics.Gauge,
+			Keys:        []string{"label"},
+			LabelValues: []*metrics.LabelValue{},
+		},
+	},
+	"bar": {
+		&metrics.Metric{
+			Name:    "bar",
+			Program: "reader_test",
+			Kind:    metrics.Counter,
+			Keys:    []string{},
+			LabelValues: []*metrics.LabelValue{
+				&metrics.LabelValue{
+					Value: datum.MakeInt(0, time.Unix(0, 0)),
+				},
 			},
 		},
 	},
-	&metrics.Metric{
-		Name:    "floaty",
-		Program: "reader_test",
-		Kind:    metrics.Gauge,
-		Type:    datum.Float,
-		Keys:    []string{},
-		LabelValues: []*metrics.LabelValue{
-			&metrics.LabelValue{
-				Value: datum.MakeFloat(37.0, time.Date(2017, 6, 15, 18, 9, 37, 0, time.UTC)),
+	"floaty": {
+		&metrics.Metric{
+			Name:    "floaty",
+			Program: "reader_test",
+			Kind:    metrics.Gauge,
+			Type:    datum.Float,
+			Keys:    []string{},
+			LabelValues: []*metrics.LabelValue{
+				&metrics.LabelValue{
+					Labels: []string{},
+					Value:  datum.MakeFloat(37.0, time.Date(2017, 6, 15, 18, 9, 37, 0, time.UTC)),
+				},
 			},
 		},
 	},
@@ -93,7 +108,7 @@ func TestReadTestData(t *testing.T) {
 	ReadTestData(f, "reader_test", store)
 	diff := deep.Equal(expectedMetrics, store.Metrics)
 	if diff != nil {
-		t.Errorf("metrics don't match: %s\n", diff)
+		t.Error(diff)
 		t.Logf("store contains %s", store.Metrics)
 	}
 }
