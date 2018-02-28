5bf5a68064bb09e0d2f2f999a7495e32ac64e588
Jamie Wilkinson <jaq@spacepants.org>
2018-03-01 09:06:30 +1100

    Add debugging output.

--- mtail_test.sh
+++ mtail_test.sh
@@ -8,13 +8,16 @@ mkdir /tmp/test/progs
 mtail --logtostderr --vmodule=tail=2,log_watcher=2 --progs /tmp/test/progs --logs /tmp/test/logs/log &
 pid=$!
 
+sleep 1
 echo 1 >> /tmp/test/logs/log
 sleep 1
+echo "Starting truncate"
 cat /dev/null > /tmp/test/logs/log
+echo "Actual truncate done"
 sleep 1
 echo 2 >> /tmp/test/logs/log
 
-sleep 1
+sleep 2
 
 wget -q -O- http://localhost:3903/debug/vars | grep count
 
