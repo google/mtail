c596367c301fa4ca3315aff4c3fe2e5b08e28341
Jamie Wilkinson <jaq@spacepants.org>
2018-03-12 21:58:49 +1100

    Adds an errorNode to the AST.
    Start a branch to improve syntax errors by putting error tokens and nodes into
    the parse tree.

--- vm/sexp.go
+++ vm/sexp.go
@@ -172,6 +172,9 @@ func (s *Sexp) VisitBefore(n astNode) Visitor {
 	case *delNode:
 		s.emit("del")
 
+	case *errorNode:
+		s.emit(fmt.Sprintf("error %q", v.spelling))
+
 	case *indexedExprNode, *stmtlistNode, *exprlistNode, *condNode, *decoDefNode, *decoNode, *patternExprNode: // normal walk
 
 	default:
