413c81490300a8db1c7f84d81f562e983ae438ff
Jamie Wilkinson <jaq@spacepants.org>
2018-09-20 09:17:12 +1000

    Fix sense of test.

--- tests/compile_only_test.sh
+++ tests/compile_only_test.sh
@@ -10,12 +10,7 @@ cat > $PROGS/bad.mtail <<EOF
 asdfadsf
 EOF
 start_server nobg --progs $PROGS --logs $LOGS/* --compile_only
-# r=$(wait ${MTAIL_PID})
-
-# if [[ $r -eq 0 ]; then
-#     fail "returned zero"
-# fi
-if ! grep -q "compile failed" ${TEST_TMPDIR}/mtail.INFO ; then
+if grep -q "compile failed" ${TEST_TMPDIR}/mtail.INFO ; then
     fail "compile failed not reported in log"
 fi
 
