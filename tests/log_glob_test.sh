65d28eb93953ae51f0c12830ffb65364dfe957c6
Jamie Wilkinson <jaq@spacepants.org>
2018-10-14 20:42:38 -0700

    clean up log_glob_test.

--- tests/log_glob_test.sh
+++ tests/log_glob_test.sh
@@ -20,7 +20,7 @@ expect_json_field_eq "1" log_count "${DATA}"
 expect_json_field_eq "1" line_count "${DATA}"
 
 echo "line 1" >> $LOGS/log1
-sleep 2
+sleep 1
 
 uri_get /debug/vars
 expect_json_field_eq "2" log_count "${DATA}"
