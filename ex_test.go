d002cd7c2f55d93b723d867ec99064a2d442d49e
Jamie Wilkinson <jaq@spacepants.org>
2018-11-26 23:43:55 -0800

    Move testutil to internal.

--- ex_test.go
+++ ex_test.go
@@ -14,9 +14,9 @@ import (
 	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/google/mtail/internal/metrics"
 	"github.com/google/mtail/internal/metrics/datum"
+	"github.com/google/mtail/internal/testutil"
 	"github.com/google/mtail/internal/watcher"
 	"github.com/google/mtail/mtail"
-	"github.com/google/mtail/testutil"
 	"github.com/spf13/afero"
 )
 
