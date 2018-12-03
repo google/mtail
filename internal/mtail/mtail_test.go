542bc6550367e812d9273678b15091f551263392
Jamie Wilkinson <jaq@spacepants.org>
2018-12-03 13:43:42 -0800

    lint

--- internal/mtail/mtail_test.go
+++ internal/mtail/mtail_test.go
@@ -713,9 +713,9 @@ func TestProgramReloadNoDuplicateMetrics(t *testing.T) {
 		if v == nil {
 			return false, nil
 		}
-		n, err := strconv.Atoi(v.String())
-		if err != nil {
-			return false, err
+		n, nerr := strconv.Atoi(v.String())
+		if nerr != nil {
+			return false, nerr
 		}
 		if n < 1 {
 			return false, nil
@@ -745,9 +745,9 @@ func TestProgramReloadNoDuplicateMetrics(t *testing.T) {
 
 	checkFoo := func() (bool, error) {
 		v := store.Metrics["foo"][0]
-		d, err := v.GetDatum()
-		if err != nil {
-			return false, err
+		d, derr := v.GetDatum()
+		if derr != nil {
+			return false, derr
 		}
 		if d.ValueString() != "1" {
 			t.Log(d)
@@ -776,9 +776,9 @@ func TestProgramReloadNoDuplicateMetrics(t *testing.T) {
 		if v == nil {
 			return false, nil
 		}
-		n, err := strconv.Atoi(v.String())
-		if err != nil {
-			return false, err
+		n, nerr := strconv.Atoi(v.String())
+		if nerr != nil {
+			return false, nerr
 		}
 		if n < 2 {
 			return false, nil
