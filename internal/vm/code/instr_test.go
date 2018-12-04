fc9681e91848538419b804711a03ffcded838cc1
Jamie Wilkinson <jaq@spacepants.org>
2018-12-04 17:48:25 +1100

    Rename, lint

--- internal/vm/code/instr_test.go
+++ internal/vm/code/instr_test.go
@@ -10,10 +10,10 @@ import (
 	"github.com/google/mtail/internal/vm/code"
 )
 
-func TestBytecodeString(t *testing.T) {
+func TestInstrString(t *testing.T) {
 	expected := "{match 0}"
 
-	if diff := testutil.Diff(code.Instr{code.Match, 0}.String(), expected); diff != "" {
-		t.Errorf("bytecodee string didn't match:\n%s", diff)
+	if diff := testutil.Diff(code.Instr{Opcode: code.Match, Operand: 0}.String(), expected); diff != "" {
+		t.Errorf("instr string didn't match:\n%s", diff)
 	}
 }
