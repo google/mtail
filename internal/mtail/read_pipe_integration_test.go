967a4967f0e75b77cdc68459ae6bdf5cb9cf4c82
Jamie Wilkinson <jaq@spacepants.org>
2019-01-10 15:29:59 +1100

    Open the named pipe after startserver to avoid a hang waiting for the mtail server.

--- internal/mtail/read_pipe_integration_test.go
+++ internal/mtail/read_pipe_integration_test.go
@@ -37,17 +37,27 @@ func TestReadFromPipe(t *testing.T) {
 	}
 	time.Sleep(time.Second)
 
+	m, stopM := mtail.TestStartServer(t, 0, false, mtail.LogPathPatterns(logDir+"/*"), mtail.ProgramPath(progDir))
+
+	defer stopM()
+
+	// TODO(jaq): Want to open the file before the startserver to the pipe
+	// exists before start, but that hangs the server.
 	f, err := os.OpenFile(logFile, os.O_RDWR|syscall.O_NONBLOCK, 0600)
 	if err != nil {
 		t.Fatal(err)
 	}
+	defer func() {
+		// TODO(jaq): Use f.SetDeadline once we stop using afero.
+		err = f.Close()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+
 	time.Sleep(time.Second)
 
-	m, stopM := mtail.TestStartServer(t, 0, false, mtail.LogPathPatterns(logDir+"/*"), mtail.ProgramPath(progDir))
-	defer stopM()
-
 	startLineCount := mtail.TestGetMetric(t, m.Addr(), "line_count")
-
 	time.Sleep(1 * time.Second)
 
 	n, err := f.WriteString("1\n2\n3\n")
@@ -58,15 +68,8 @@ func TestReadFromPipe(t *testing.T) {
 	time.Sleep(1 * time.Second)
 
 	endLineCount := mtail.TestGetMetric(t, m.Addr(), "line_count")
-
 	lineCount := endLineCount.(float64) - startLineCount.(float64)
 	if lineCount != 3. {
 		t.Errorf("output didn't have expected line count increase: want 3 got %#v", lineCount)
-	}
-
-	// TODO(jaq): Use f.SetDeadline once we stop using afero.
-	err = f.Close()
-	if err != nil {
-		t.Fatal(err)
 	}
 }
