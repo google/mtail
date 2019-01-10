e137801355d62b097189e09254583f547a517db6
Jamie Wilkinson <jaq@spacepants.org>
2019-01-10 14:03:21 +1100

    Convert the partial line read test to Go.

---
+++ internal/mtail/partial_line_integration_test.go
@@ -0,0 +1,78 @@
+// +build integration
+
+package mtail_test
+
+import (
+	"os"
+	"path"
+	"testing"
+	"time"
+
+	"github.com/golang/glog"
+	"github.com/google/mtail/internal/mtail"
+)
+
+func TestPartialLineRead(t *testing.T) {
+	tmpDir, rmTmpDir := mtail.TestTempDir(t)
+	defer rmTmpDir()
+
+	logDir := path.Join(tmpDir, "logs")
+	progDir := path.Join(tmpDir, "progs")
+	err := os.Mkdir(logDir, 0700)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = os.Mkdir(progDir, 0700)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	logFile := path.Join(logDir, "log")
+
+	f := mtail.TestOpenFile(t, logFile)
+
+	m, stopM := mtail.TestStartServer(t, 0, false, mtail.ProgramPath(progDir), mtail.LogPathPatterns(logDir+"/log"))
+	defer stopM()
+
+	startLineCount := mtail.TestGetMetric(t, m.Addr(), "line_count")
+
+	{
+		n, err := f.WriteString("line 1\n")
+		if err != nil {
+			t.Fatal(err)
+		}
+		glog.Infof("Wrote %d bytes", n)
+		time.Sleep(time.Second)
+
+		lineCount := mtail.TestGetMetric(t, m.Addr(), "line_count")
+
+		mtail.ExpectMetricDelta(t, lineCount, startLineCount, 1)
+	}
+
+	{
+
+		n, err := f.WriteString("line ")
+		if err != nil {
+			t.Fatal(err)
+		}
+		glog.Infof("Wrote %d bytes", n)
+		time.Sleep(time.Second)
+
+		lineCount := mtail.TestGetMetric(t, m.Addr(), "line_count")
+
+		mtail.ExpectMetricDelta(t, lineCount, startLineCount, 1)
+	}
+	{
+
+		n, err := f.WriteString("2\n")
+		if err != nil {
+			t.Fatal(err)
+		}
+		glog.Infof("Wrote %d bytes", n)
+		time.Sleep(time.Second)
+
+		lineCount := mtail.TestGetMetric(t, m.Addr(), "line_count")
+
+		mtail.ExpectMetricDelta(t, lineCount, startLineCount, 2)
+	}
+}
