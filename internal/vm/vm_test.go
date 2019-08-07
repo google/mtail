9ca662b1d6b974bc56b1ada89f9329aa8e0ffa68
Jamie Wilkinson <jaq@spacepants.org>
2019-08-07 20:58:45 +1000

    Create instructions with a reference to their original source line number.

--- internal/vm/vm_test.go
+++ internal/vm/vm_test.go
@@ -28,7 +28,7 @@ var instructions = []struct {
 	expectedThread thread
 }{
 	{"match",
-		code.Instr{code.Match, 0},
+		code.Instr{code.Match, 0, 0},
 		[]*regexp.Regexp{regexp.MustCompile("a*b")},
 		[]string{},
 		[]interface{}{},
@@ -36,126 +36,126 @@ var instructions = []struct {
 		thread{pc: 0, matches: map[int][]string{0: {"aaaab"}}},
 	},
 	{"cmp lt",
-		code.Instr{code.Cmp, -1},
+		code.Instr{code.Cmp, -1, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{1, "2"},
 		[]interface{}{true},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"cmp eq",
-		code.Instr{code.Cmp, 0},
+		code.Instr{code.Cmp, 0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{"2", "2"},
 		[]interface{}{true},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"cmp gt",
-		code.Instr{code.Cmp, 1},
+		code.Instr{code.Cmp, 1, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{2, 1},
 		[]interface{}{true},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"cmp le",
-		code.Instr{code.Cmp, 1},
+		code.Instr{code.Cmp, 1, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{2, "2"},
 		[]interface{}{false},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"cmp ne",
-		code.Instr{code.Cmp, 0},
+		code.Instr{code.Cmp, 0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{"1", "2"},
 		[]interface{}{false},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"cmp ge",
-		code.Instr{code.Cmp, -1},
+		code.Instr{code.Cmp, -1, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{2, 2},
 		[]interface{}{false},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"cmp gt float float",
-		code.Instr{code.Cmp, 1},
+		code.Instr{code.Cmp, 1, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{"2.0", "1.0"},
 		[]interface{}{true},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"cmp gt float int",
-		code.Instr{code.Cmp, 1},
+		code.Instr{code.Cmp, 1, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{"1.0", "2"},
 		[]interface{}{false},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"cmp gt int float",
-		code.Instr{code.Cmp, 1},
+		code.Instr{code.Cmp, 1, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{"1", "2.0"},
 		[]interface{}{false},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"cmp eq string string false",
-		code.Instr{code.Cmp, 0},
+		code.Instr{code.Cmp, 0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{"abc", "def"},
 		[]interface{}{false},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"cmp eq string string true",
-		code.Instr{code.Cmp, 0},
+		code.Instr{code.Cmp, 0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{"abc", "abc"},
 		[]interface{}{true},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"cmp gt float float",
-		code.Instr{code.Cmp, 1},
+		code.Instr{code.Cmp, 1, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{2.0, 1.0},
 		[]interface{}{true},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"cmp gt float int",
-		code.Instr{code.Cmp, 1},
+		code.Instr{code.Cmp, 1, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{1.0, 2},
 		[]interface{}{false},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"cmp gt int float",
-		code.Instr{code.Cmp, 1},
+		code.Instr{code.Cmp, 1, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{1, 2.0},
 		[]interface{}{false},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"jnm",
-		code.Instr{code.Jnm, 37},
+		code.Instr{code.Jnm, 37, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{false},
 		[]interface{}{},
 		thread{pc: 37, matches: map[int][]string{}}},
 	{"jm",
-		code.Instr{code.Jm, 37},
+		code.Instr{code.Jm, 37, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{false},
 		[]interface{}{},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"jmp",
-		code.Instr{code.Jmp, 37},
+		code.Instr{code.Jmp, 37, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{},
 		[]interface{}{},
 		thread{pc: 37, matches: map[int][]string{}}},
 	{"strptime",
-		code.Instr{code.Strptime, 0},
+		code.Instr{code.Strptime, 0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{"2012/01/18 06:25:00", "2006/01/02 15:04:05"},
@@ -163,294 +163,294 @@ var instructions = []struct {
 		thread{pc: 0, time: time.Date(2012, 1, 18, 6, 25, 0, 0, time.UTC),
 			matches: map[int][]string{}}},
 	{"iadd",
-		code.Instr{code.Iadd, 0},
+		code.Instr{code.Iadd, 0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{2, 1},
 		[]interface{}{int64(3)},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"isub",
-		code.Instr{code.Isub, 0},
+		code.Instr{code.Isub, 0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{2, 1},
 		[]interface{}{int64(1)},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"imul",
-		code.Instr{code.Imul, 0},
+		code.Instr{code.Imul, 0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{2, 1},
 		[]interface{}{int64(2)},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"idiv",
-		code.Instr{code.Idiv, 0},
+		code.Instr{code.Idiv, 0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{4, 2},
 		[]interface{}{int64(2)},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"imod",
-		code.Instr{code.Imod, 0},
+		code.Instr{code.Imod, 0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{4, 2},
 		[]interface{}{int64(0)},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"imod 2",
-		code.Instr{code.Imod, 0},
+		code.Instr{code.Imod, 0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{3, 2},
 		[]interface{}{int64(1)},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"tolower",
-		code.Instr{code.Tolower, 0},
+		code.Instr{code.Tolower, 0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{"mIxeDCasE"},
 		[]interface{}{"mixedcase"},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"length",
-		code.Instr{code.Length, 0},
+		code.Instr{code.Length, 0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{"1234"},
 		[]interface{}{4},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"length 0",
-		code.Instr{code.Length, 0},
+		code.Instr{code.Length, 0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{""},
 		[]interface{}{0},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"shl",
-		code.Instr{code.Shl, 0},
+		code.Instr{code.Shl, 0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{2, 1},
 		[]interface{}{int64(4)},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"shr",
-		code.Instr{code.Shr, 0},
+		code.Instr{code.Shr, 0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{2, 1},
 		[]interface{}{int64(1)},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"and",
-		code.Instr{code.And, 0},
+		code.Instr{code.And, 0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{2, 1},
 		[]interface{}{int64(0)},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"or",
-		code.Instr{code.Or, 0},
+		code.Instr{code.Or, 0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{2, 1},
 		[]interface{}{int64(3)},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"xor",
-		code.Instr{code.Xor, 0},
+		code.Instr{code.Xor, 0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{2, 1},
 		[]interface{}{int64(3)},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"xor 2",
-		code.Instr{code.Xor, 0},
+		code.Instr{code.Xor, 0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{2, 3},
 		[]interface{}{int64(1)},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"xor 3",
-		code.Instr{code.Xor, 0},
+		code.Instr{code.Xor, 0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{-1, 3},
 		[]interface{}{int64(^3)},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"neg",
-		code.Instr{code.Neg, 0},
+		code.Instr{code.Neg, 0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{0},
 		[]interface{}{int64(-1)},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"not",
-		code.Instr{code.Not, 0},
+		code.Instr{code.Not, 0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{false},
 		[]interface{}{true},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"pow",
-		code.Instr{code.Ipow, 0},
+		code.Instr{code.Ipow, 0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{2, 2},
 		[]interface{}{int64(4)},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"s2i pop",
-		code.Instr{code.S2i, 1},
+		code.Instr{code.S2i, 1, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{"ff", 16},
 		[]interface{}{int64(255)},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"s2i",
-		code.Instr{code.S2i, nil},
+		code.Instr{code.S2i, nil, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{"190"},
 		[]interface{}{int64(190)},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"s2f",
-		code.Instr{code.S2f, nil},
+		code.Instr{code.S2f, nil, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{"1.0"},
 		[]interface{}{float64(1.0)},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"i2f",
-		code.Instr{code.I2f, nil},
+		code.Instr{code.I2f, nil, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{1},
 		[]interface{}{float64(1.0)},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"settime",
-		code.Instr{code.Settime, 0},
+		code.Instr{code.Settime, 0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{int64(0)},
 		[]interface{}{},
 		thread{pc: 0, time: time.Unix(0, 0).UTC(), matches: map[int][]string{}}},
 	{"push int",
-		code.Instr{code.Push, 1},
+		code.Instr{code.Push, 1, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{},
 		[]interface{}{1},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"push float",
-		code.Instr{code.Push, 1.0},
+		code.Instr{code.Push, 1.0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{},
 		[]interface{}{1.0},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"setmatched false",
-		code.Instr{code.Setmatched, false},
+		code.Instr{code.Setmatched, false, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{},
 		[]interface{}{},
 		thread{matched: false, pc: 0, matches: map[int][]string{}}},
 	{"setmatched true",
-		code.Instr{code.Setmatched, true},
+		code.Instr{code.Setmatched, true, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{},
 		[]interface{}{},
 		thread{matched: true, pc: 0, matches: map[int][]string{}}},
 	{"otherwise",
-		code.Instr{code.Otherwise, nil},
+		code.Instr{code.Otherwise, nil, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{},
 		[]interface{}{true},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"fadd",
-		code.Instr{code.Fadd, nil},
+		code.Instr{code.Fadd, nil, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{1.0, 2.0},
 		[]interface{}{3.0},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"fsub",
-		code.Instr{code.Fsub, nil},
+		code.Instr{code.Fsub, nil, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{1.0, 2.0},
 		[]interface{}{-1.0},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"fmul",
-		code.Instr{code.Fmul, nil},
+		code.Instr{code.Fmul, nil, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{1.0, 2.0},
 		[]interface{}{2.0},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"fdiv",
-		code.Instr{code.Fdiv, nil},
+		code.Instr{code.Fdiv, nil, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{1.0, 2.0},
 		[]interface{}{0.5},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"fmod",
-		code.Instr{code.Fmod, nil},
+		code.Instr{code.Fmod, nil, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{1.0, 2.0},
 		[]interface{}{1.0},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"fpow",
-		code.Instr{code.Fpow, nil},
+		code.Instr{code.Fpow, nil, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{2.0, 2.0},
 		[]interface{}{4.0},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"getfilename",
-		code.Instr{code.Getfilename, nil},
+		code.Instr{code.Getfilename, nil, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{},
 		[]interface{}{testFilename},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"i2s",
-		code.Instr{code.I2s, nil},
+		code.Instr{code.I2s, nil, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{1},
 		[]interface{}{"1"},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"f2s",
-		code.Instr{code.F2s, nil},
+		code.Instr{code.F2s, nil, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{3.1},
 		[]interface{}{"3.1"},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"cat",
-		code.Instr{code.Cat, 0},
+		code.Instr{code.Cat, 0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{"first", "second"},
 		[]interface{}{"firstsecond"},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"icmp gt false",
-		code.Instr{code.Icmp, 1},
+		code.Instr{code.Icmp, 1, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{1, 2},
 		[]interface{}{false},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"fcmp gt false",
-		code.Instr{code.Fcmp, 1},
+		code.Instr{code.Fcmp, 1, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{1.0, 2.0},
 		[]interface{}{false},
 		thread{pc: 0, matches: map[int][]string{}}},
 	{"scmp eq false",
-		code.Instr{code.Scmp, 0},
+		code.Instr{code.Scmp, 0, 0},
 		[]*regexp.Regexp{},
 		[]string{},
 		[]interface{}{"abc", "def"},
@@ -526,7 +526,7 @@ func TestDatumSetInstrs(t *testing.T) {
 	)
 
 	// simple inc
-	v := makeVM(code.Instr{code.Inc, nil}, m)
+	v := makeVM(code.Instr{code.Inc, nil, 0}, m)
 	d, err := m[0].GetDatum()
 	if err != nil {
 		t.Fatal(err)
@@ -544,7 +544,7 @@ func TestDatumSetInstrs(t *testing.T) {
 		t.Errorf("Unexpected value %v", d)
 	}
 	// inc by int
-	v = makeVM(code.Instr{code.Inc, 0}, m)
+	v = makeVM(code.Instr{code.Inc, 0, 0}, m)
 	d, err = m[0].GetDatum()
 	if err != nil {
 		t.Fatal(err)
@@ -563,7 +563,7 @@ func TestDatumSetInstrs(t *testing.T) {
 		t.Errorf("Unexpected value %v", d)
 	}
 	// inc by str
-	v = makeVM(code.Instr{code.Inc, 0}, m)
+	v = makeVM(code.Instr{code.Inc, 0, 0}, m)
 	d, err = m[0].GetDatum()
 	if err != nil {
 		t.Fatal(err)
@@ -582,7 +582,7 @@ func TestDatumSetInstrs(t *testing.T) {
 		t.Errorf("Unexpected value %v", d)
 	}
 	// iset
-	v = makeVM(code.Instr{code.Iset, nil}, m)
+	v = makeVM(code.Instr{code.Iset, nil, 0}, m)
 	d, err = m[0].GetDatum()
 	if err != nil {
 		t.Fatal(err)
@@ -601,7 +601,7 @@ func TestDatumSetInstrs(t *testing.T) {
 		t.Errorf("Unexpected value %v", d)
 	}
 	// iset str
-	v = makeVM(code.Instr{code.Iset, nil}, m)
+	v = makeVM(code.Instr{code.Iset, nil, 0}, m)
 	d, err = m[0].GetDatum()
 	if err != nil {
 		t.Fatal(err)
@@ -620,7 +620,7 @@ func TestDatumSetInstrs(t *testing.T) {
 		t.Errorf("Unexpected value %v", d)
 	}
 	// fset
-	v = makeVM(code.Instr{code.Fset, nil}, m)
+	v = makeVM(code.Instr{code.Fset, nil, 0}, m)
 	d, err = m[1].GetDatum()
 	if err != nil {
 		t.Fatal(err)
@@ -639,7 +639,7 @@ func TestDatumSetInstrs(t *testing.T) {
 		t.Errorf("Unexpected value %v", d)
 	}
 	// fset str
-	v = makeVM(code.Instr{code.Fset, nil}, m)
+	v = makeVM(code.Instr{code.Fset, nil, 0}, m)
 	d, err = m[1].GetDatum()
 	if err != nil {
 		t.Fatal(err)
@@ -659,7 +659,7 @@ func TestDatumSetInstrs(t *testing.T) {
 	}
 
 	// sset
-	v = makeVM(code.Instr{code.Sset, nil}, m)
+	v = makeVM(code.Instr{code.Sset, nil, 0}, m)
 	d, err = m[2].GetDatum()
 	if err != nil {
 		t.Fatal(err)
@@ -679,7 +679,7 @@ func TestDatumSetInstrs(t *testing.T) {
 	}
 
 	// dec
-	v = makeVM(code.Instr{code.Dec, nil}, m)
+	v = makeVM(code.Instr{code.Dec, nil, 0}, m)
 	d, err = m[0].GetDatum()
 	if err != nil {
 		t.Fatal(err)
@@ -699,7 +699,7 @@ func TestDatumSetInstrs(t *testing.T) {
 	}
 
 	// set hist
-	v = makeVM(code.Instr{code.Fset, nil}, m)
+	v = makeVM(code.Instr{code.Fset, nil, 0}, m)
 	d, err = m[3].GetDatum()
 	testutil.FatalIfErr(t, err)
 	v.t.Push(d)
@@ -717,7 +717,7 @@ func TestDatumSetInstrs(t *testing.T) {
 
 func TestStrptimeWithTimezone(t *testing.T) {
 	loc, _ := time.LoadLocation("Europe/Berlin")
-	obj := &object.Object{Program: []code.Instr{{code.Strptime, 0}}}
+	obj := &object.Object{Program: []code.Instr{{code.Strptime, 0, 0}}}
 	vm := New("strptimezone", obj, true, loc)
 	vm.t = new(thread)
 	vm.t.stack = make([]interface{}, 0)
@@ -730,7 +730,7 @@ func TestStrptimeWithTimezone(t *testing.T) {
 }
 
 func TestStrptimeWithoutTimezone(t *testing.T) {
-	obj := &object.Object{Program: []code.Instr{{code.Strptime, 0}}}
+	obj := &object.Object{Program: []code.Instr{{code.Strptime, 0, 0}}}
 	vm := New("strptimezone", obj, true, nil)
 	vm.t = new(thread)
 	vm.t.stack = make([]interface{}, 0)
@@ -752,7 +752,7 @@ func TestDatumFetchInstrs(t *testing.T) {
 
 	{
 		// iget
-		v := makeVM(code.Instr{code.Iget, nil}, m)
+		v := makeVM(code.Instr{code.Iget, nil, 0}, m)
 		d, err := m[0].GetDatum()
 		if err != nil {
 			t.Fatal(err)
@@ -774,7 +774,7 @@ func TestDatumFetchInstrs(t *testing.T) {
 
 	{
 		// fget
-		v := makeVM(code.Instr{code.Fget, nil}, m)
+		v := makeVM(code.Instr{code.Fget, nil, 0}, m)
 		d, err := m[1].GetDatum()
 		if err != nil {
 			t.Fatal(err)
@@ -796,7 +796,7 @@ func TestDatumFetchInstrs(t *testing.T) {
 
 	{
 		// sget
-		v := makeVM(code.Instr{code.Sget, nil}, m)
+		v := makeVM(code.Instr{code.Sget, nil, 0}, m)
 		d, err := m[2].GetDatum()
 		if err != nil {
 			t.Fatal(err)
@@ -826,7 +826,7 @@ func TestDeleteInstrs(t *testing.T) {
 	_, err := m[0].GetDatum("z")
 	testutil.FatalIfErr(t, err)
 
-	v := makeVM(code.Instr{code.Expire, 1}, m)
+	v := makeVM(code.Instr{code.Expire, 1, 0}, m)
 	v.t.Push(time.Hour)
 	v.t.Push("z")
 	v.t.Push(m[0])
@@ -846,7 +846,7 @@ func TestDeleteInstrs(t *testing.T) {
 func TestTimestampInstr(t *testing.T) {
 	var m []*metrics.Metric
 	now := time.Now().UTC()
-	v := makeVM(code.Instr{code.Timestamp, nil}, m)
+	v := makeVM(code.Instr{code.Timestamp, nil, 0}, m)
 	v.execute(v.t, v.prog[0])
 	if v.terminate {
 		t.Fatal("execution failed, see info log")
