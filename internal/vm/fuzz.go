0176e2c78ff6058b9a2febd705cd14d9bd4b236b
Jamie Wilkinson <jaq@google.com>
2019-08-27 15:29:49 +1000

    Dump debug only when debugging.

--- internal/vm/fuzz.go
+++ internal/vm/fuzz.go
@@ -12,12 +12,16 @@ import (
 )
 
 func Fuzz(data []byte) int {
+	// Enable this when debugging with a fuzz crash artifact.
+	dumpDebug := false
 	// We need to successfully parse flags to initialize the glog logger used
 	// by the compiler, but the fuzzer gets called with flags captured by the
 	// libfuzzer main, which we don't want to intercept here.
 	flag.CommandLine = flag.NewFlagSet("", flag.ContinueOnError)
-	if _, err := Compile("fuzz", bytes.NewReader(data), false, false, false, nil); err != nil {
-		fmt.Print(err)
+	if _, err := Compile("fuzz", bytes.NewReader(data), dumpDebug, dumpDebug, false, nil); err != nil {
+		if dumpDebug {
+			fmt.Print(err)
+		}
 		return 0
 	}
 	return 1
