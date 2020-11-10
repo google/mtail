71e23fa73390f039876a093a90a890d7a3351727
Jamie Wilkinson <jaq@spacepants.org>
2020-11-10 19:37:34 +1100

    Move another glob-and-ignore test to the glob integration test file.

--- internal/mtail/log_glob_integration_test.go
+++ internal/mtail/log_glob_integration_test.go
@@ -4,6 +4,7 @@
 package mtail_test
 
 import (
+	"os"
 	"path"
 	"sync"
 	"testing"
@@ -107,4 +108,61 @@ func TestGlobAfterStart(t *testing.T) {
 		logCountCheck()
 	}()
 	wg.Wait()
+}
+
+func TestGlobIgnoreFolder(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode")
+	}
+
+	workdir, rmWorkdir := testutil.TestTempDir(t)
+	defer rmWorkdir()
+
+	globTests := []struct {
+		name     string
+		isFolder bool
+		expected bool
+	}{
+		{
+			path.Join(workdir, "log1"),
+			false,
+			true,
+		},
+		{
+			path.Join(workdir, "logarchive"),
+			true,
+			false,
+		},
+		{
+			path.Join(workdir, "log2.gz"),
+			false,
+			false,
+		},
+	}
+	count := 0
+	for _, tt := range globTests {
+		var err error
+		var log *os.File
+
+		if tt.isFolder {
+			err = os.Mkdir(tt.name, 0700)
+			testutil.FatalIfErr(t, err)
+			continue
+		} else {
+			log, err = os.Create(tt.name)
+		}
+
+		if !tt.isFolder && tt.expected {
+			count++
+		}
+		defer log.Close()
+		testutil.FatalIfErr(t, err)
+		testutil.WriteString(t, log, "\n")
+	}
+	m, stopM := mtail.TestStartServer(t, 0, false, mtail.LogPathPatterns(path.Join(workdir, "log*")), mtail.IgnoreRegexPattern("\\.gz"))
+	defer stopM()
+
+	if r := m.GetMetric("log_count"); r != float64(count) {
+		t.Errorf("Expecting log Count for %d, received %g", count, r)
+	}
 }
