91c771d541f6ed64a8837799f285c3d5f962b669
Jamie Wilkinson <jaq@spacepants.org>
2020-11-11 16:55:19 +1100

    Remove obsolete test-only code.

--- internal/mtail/mtail_test.go
+++ internal/mtail/mtail_test.go
@@ -4,40 +4,10 @@
 package mtail
 
 import (
-	"expvar"
 	"fmt"
 	"runtime"
-	"strings"
 	"testing"
-
-	"github.com/google/mtail/internal/metrics"
-	"github.com/google/mtail/internal/testutil"
-	"github.com/google/mtail/internal/watcher"
 )
-
-const testProgram = "/$/ { }\n"
-
-func startMtailServer(t *testing.T, options ...func(*Server) error) *Server {
-	expvar.Get("lines_total").(*expvar.Int).Set(0)
-	expvar.Get("log_count").(*expvar.Int).Set(0)
-	expvar.Get("log_rotations_total").(*expvar.Map).Init()
-	expvar.Get("prog_loads_total").(*expvar.Map).Init()
-
-	w, err := watcher.NewLogWatcher(0, true)
-	if err != nil {
-		t.Errorf("Couodn't make a log watcher: %s", err)
-	}
-	m, err := New(metrics.NewStore(), w, options...)
-	testutil.FatalIfErr(t, err)
-	if pErr := m.l.CompileAndRun("test", strings.NewReader(testProgram)); pErr != nil {
-		t.Errorf("Couldn't compile program: %s", pErr)
-	}
-
-	if err := m.StartTailing(); err != nil {
-		t.Errorf("StartTailing failed: %s", err)
-	}
-	return m
-}
 
 func TestBuildInfo(t *testing.T) {
 	buildInfo := BuildInfo{
