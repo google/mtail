82adca94c38bfe1ef2fd4b13fc4d67a97aee3bb2
Jamie Wilkinson <jaq@spacepants.org>
2020-11-11 20:46:26 +1100

    Reenable log watcher tests.
    Correctly determine file-not-found when adding a watch, and repair the test to
    simulate a processor adding more watches.

--- internal/watcher/log_watcher_test.go
+++ internal/watcher/log_watcher_test.go
@@ -9,27 +9,23 @@ import (
 	"path"
 	"path/filepath"
 	"testing"
-	"time"
 
 	"github.com/google/mtail/internal/testutil"
 )
 
 type testStubProcessor struct {
-	Events chan Event
+	Events []Event
 }
 
 func (t *testStubProcessor) ProcessFileEvent(ctx context.Context, e Event) {
-	go func() {
-		t.Events <- e
-	}()
+	t.Events = append(t.Events, e)
 }
 
 func newStubProcessor() *testStubProcessor {
-	return &testStubProcessor{Events: make(chan Event, 1)}
+	return &testStubProcessor{Events: make([]Event, 0)}
 }
 
 func TestLogWatcher(t *testing.T) {
-	t.Skip("busted ")
 	testutil.SkipIfShort(t)
 
 	workdir, rmWorkdir := testutil.TestTempDir(t)
@@ -38,97 +34,47 @@ func TestLogWatcher(t *testing.T) {
 	w, err := NewLogWatcher(0)
 	testutil.FatalIfErr(t, err)
 	defer func() {
-		if err = w.Close(); err != nil {
-			t.Fatal(err)
-		}
+		testutil.FatalIfErr(t, w.Close())
 	}()
 
 	s := newStubProcessor()
+	expected := []Event{}
 
-	if err = w.Observe(workdir, s); err != nil {
-		t.Fatal(err)
-	}
-	f, err := os.Create(filepath.Join(workdir, "logfile"))
-	testutil.FatalIfErr(t, err)
+	testutil.FatalIfErr(t, w.Observe(workdir, s))
+
+	f := testutil.TestOpenFile(t, filepath.Join(workdir, "logfile"))
 	w.Poll()
-	select {
-	case e := <-s.Events:
-		expected := Event{Create, filepath.Join(workdir, "logfile")}
-		testutil.ExpectNoDiff(t, expected, e)
-	case <-time.After(100 * time.Millisecond):
-		t.Fatal("no event received before timeout")
-	}
+	expected = append(expected, Event{Create, filepath.Join(workdir, "logfile")})
+	testutil.ExpectNoDiff(t, expected, s.Events)
+	// Simulate the processor observing the new log
+	testutil.FatalIfErr(t, w.Observe(filepath.Join(workdir, "logfile"), s))
 
-	n, err := f.WriteString("hi")
-	testutil.FatalIfErr(t, err)
-	if n != 2 {
-		t.Fatalf("wrote %d instead of 2", n)
-	}
+	testutil.WriteString(t, f, "hi")
 	testutil.FatalIfErr(t, f.Close())
 	w.Poll()
-	select {
-	case e := <-s.Events:
-		expected := Event{Update, filepath.Join(workdir, "logfile")}
-		testutil.ExpectNoDiff(t, expected, e)
-	case <-time.After(100 * time.Millisecond):
-		t.Fatal("no event received before timeout")
-	}
+	expected = append(expected, Event{Update, filepath.Join(workdir, "logfile")})
+	testutil.ExpectNoDiff(t, expected, s.Events)
 
 	testutil.FatalIfErr(t, os.Rename(filepath.Join(workdir, "logfile"), filepath.Join(workdir, "logfile2")))
 	w.Poll()
-	results := make([]Event, 0)
-	for i := 0; i < 2; i++ {
-		select {
-		case e := <-s.Events:
-			results = append(results, e)
-		case <-time.After(100 * time.Millisecond):
-			t.Fatal("no event received before timeout")
-		}
-	}
-	expected := []Event{
-		{Create, filepath.Join(workdir, "logfile2")},
-		{Delete, filepath.Join(workdir, "logfile")},
-	}
-	sorter := func(a, b Event) bool {
-		if a.Op < b.Op {
-			return true
-		}
-		if a.Op > b.Op {
-			return false
-		}
-		if a.Pathname < b.Pathname {
-			return true
-		}
-		if a.Pathname > b.Pathname {
-			return false
-		}
-		return true
-	}
-	testutil.ExpectNoDiff(t, expected, results, testutil.SortSlices(sorter))
+	expected = append(expected, Event{Create, filepath.Join(workdir, "logfile2")},
+		Event{Delete, filepath.Join(workdir, "logfile")})
+	testutil.ExpectNoDiff(t, expected, s.Events)
+	// Simulate watch on logfile2
+	testutil.FatalIfErr(t, w.Observe(filepath.Join(workdir, "logfile2"), s))
 
 	testutil.FatalIfErr(t, os.Chmod(filepath.Join(workdir, "logfile2"), os.ModePerm))
 	w.Poll()
-	select {
-	case e := <-s.Events:
-		expected := Event{Update, filepath.Join(workdir, "logfile2")}
-		testutil.ExpectNoDiff(t, expected, e)
-	case <-time.After(100 * time.Millisecond):
-		t.Fatal("no event received before timeout")
-	}
+	expected = append(expected, Event{Update, filepath.Join(workdir, "logfile2")})
+	testutil.ExpectNoDiff(t, expected, s.Events)
 
 	testutil.FatalIfErr(t, os.Remove(filepath.Join(workdir, "logfile2")))
 	w.Poll()
-	select {
-	case e := <-s.Events:
-		expected := Event{Delete, filepath.Join(workdir, "logfile2")}
-		testutil.ExpectNoDiff(t, expected, e)
-	case <-time.After(100 * time.Millisecond):
-		t.Fatal("no event received before timeout")
-	}
+	expected = append(expected, Event{Delete, filepath.Join(workdir, "logfile2")})
+	testutil.ExpectNoDiff(t, expected, s.Events)
 }
 
-func TestLogWatcherAddError(t *testing.T) {
-	t.Skip("error injection not working")
+func TestLogWatcherAddNotFound(t *testing.T) {
 	testutil.SkipIfShort(t)
 	workdir, rmWorkdir := testutil.TestTempDir(t)
 	defer rmWorkdir()
@@ -136,9 +82,7 @@ func TestLogWatcherAddError(t *testing.T) {
 	w, err := NewLogWatcher(0)
 	testutil.FatalIfErr(t, err)
 	defer func() {
-		if err = w.Close(); err != nil {
-			t.Fatal(err)
-		}
+		testutil.FatalIfErr(t, w.Close())
 	}()
 	s := &stubProcessor{}
 	filename := filepath.Join(workdir, "test")
@@ -148,7 +92,7 @@ func TestLogWatcherAddError(t *testing.T) {
 	}
 }
 
-func TestLogWatcherAddWhilePermissionDenied(t *testing.T) {
+func TestLogWatcherAddPermissionDenied(t *testing.T) {
 	testutil.SkipIfShort(t)
 
 	// Can't force a permission denied error if run as root.
@@ -160,9 +104,7 @@ func TestLogWatcherAddWhilePermissionDenied(t *testing.T) {
 	w, err := NewLogWatcher(0)
 	testutil.FatalIfErr(t, err)
 	defer func() {
-		if err = w.Close(); err != nil {
-			t.Fatal(err)
-		}
+		testutil.FatalIfErr(t, w.Close())
 	}()
 
 	filename := filepath.Join(workdir, "test")
@@ -181,16 +123,18 @@ func TestLogWatcherAddWhilePermissionDenied(t *testing.T) {
 
 func TestWatcherNewFile(t *testing.T) {
 	testutil.SkipIfShort(t)
-	w, err := NewLogWatcher(0)
-	testutil.FatalIfErr(t, err)
+
 	tmpDir, rmTmpDir := testutil.TestTempDir(t)
 	defer rmTmpDir()
+
+	w, err := NewLogWatcher(0)
+	testutil.FatalIfErr(t, err)
+
 	s := &stubProcessor{}
 	testutil.FatalIfErr(t, w.Observe(tmpDir, s))
 	testutil.TestOpenFile(t, path.Join(tmpDir, "log"))
 	w.Poll()
 	w.Close()
 	expected := []Event{{Op: Create, Pathname: path.Join(tmpDir, "log")}}
-	// Fetching the start of the slice is a hack because we get duplicate events.
-	testutil.ExpectNoDiff(t, expected, s.Events[0:1])
+	testutil.ExpectNoDiff(t, expected, s.Events)
 }
