5245eb13e792b570047f501d3583243b1563bcc2
Jamie Wilkinson <jaq@spacepants.org>
2020-11-11 16:54:23 +1100

    Uncomment and move the log delete test to integration test.
    Leave disabled with t.Skip instead and update the docs on why not to comment
    out tests.

--- docs/Testing.md
+++ docs/Testing.md
@@ -52,9 +52,11 @@ The `TestExamplePrograms` behaves like the `one_shot` flag, and
 
 Use the `testutil` module where possible.
 
-Do not use time.Sleep; poll for events.  Even integration tests which write to disk should be fast and not require sleeps to synchronise.
+Do not use time.Sleep; poll for events.  The `TestServer` provides a `PollWatched()` method for this purpose.  Even integration tests which write to disk can be fast and not require sleeps to synchronise.
 
-Use `testing.Short()` tests in tests with disk access so that the `make smoke` command is fast.
+Use the `if testing.Short()` signal in tests with disk access so that the `make smoke` command is fast.
+
+Do not comment out tests, prefer to use the t.Skip() method indicating why it's not working if a test needs to be disabled.  This keeps them visible and compilable.
 
 # Troubleshooting
 
---
+++ internal/mtail/log_delete_integration_test.go
@@ -0,0 +1,38 @@
+// Copyright 2020 Google Inc. All Rights Reserved.
+// This file is available under the Apache license.
+
+package mtail_test
+
+import (
+	"os"
+	"path"
+	"testing"
+
+	"github.com/google/mtail/internal/mtail"
+	"github.com/google/mtail/internal/testutil"
+)
+
+func TestHandleLogDeletes(t *testing.T) {
+	t.Skip("broken, was commented out")
+	if testing.Short() {
+		t.Skip("skipping test in short mode")
+	}
+	workdir, rmWorkdir := testutil.TestTempDir(t)
+	defer rmWorkdir()
+
+	// touch log file
+	logFilepath := path.Join(workdir, "log")
+	logFile := testutil.TestOpenFile(t, logFilepath)
+	defer logFile.Close()
+
+	m, stopM := mtail.TestStartServer(t, 0, false, mtail.LogPathPatterns(logFilepath))
+	defer stopM()
+
+	logCountCheck := m.ExpectMetricDeltaWithDeadline("log_count", -1)
+
+	testutil.FatalIfErr(t, os.Remove(logFilepath))
+
+	m.PollWatched()
+
+	logCountCheck()
+}
