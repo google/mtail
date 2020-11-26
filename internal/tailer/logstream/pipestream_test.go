de4dc4ac73f0455840e2f8a00fc954cad3a5da77
Jamie Wilkinson <jaq@spacepants.org>
2020-11-27 08:18:00 +1100

    Add a test for the pipestream.

---
+++ internal/tailer/logstream/pipestream_test.go
@@ -0,0 +1,49 @@
+// Copyright 2020 Google Inc. All Rights Reserved.
+// This file is available under the Apache license.
+
+package logstream_test
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+	"sync"
+	"testing"
+
+	"github.com/google/mtail/internal/logline"
+	"github.com/google/mtail/internal/tailer/logstream"
+	"github.com/google/mtail/internal/testutil"
+	"golang.org/x/sys/unix"
+)
+
+func TestPipeStreamPoll(t *testing.T) {
+	var wg sync.WaitGroup
+
+	tmpDir, rmTmpDir := testutil.TestTempDir(t)
+	defer rmTmpDir()
+
+	name := filepath.Join(tmpDir, "fifo")
+	testutil.FatalIfErr(t, unix.Mkfifo(name, 0666))
+
+	f, err := os.OpenFile(name, os.O_RDWR, os.ModeNamedPipe)
+	testutil.FatalIfErr(t, err)
+
+	sp := NewStubProcessor()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	ps, err := logstream.New(ctx, &wg, name, sp)
+	testutil.FatalIfErr(t, err)
+
+	sp.ExpectLinesReceived(1)
+	testutil.WriteString(t, f, "1\n")
+	ps.Poll()
+
+	sp.Verify()
+	expected := []logline.LogLine{
+		{context.TODO(), name, "1"},
+	}
+	testutil.ExpectNoDiff(t, expected, sp.Result(), testutil.IgnoreFields(logline.LogLine{}, "Context"))
+
+	cancel()
+	wg.Wait()
+}
