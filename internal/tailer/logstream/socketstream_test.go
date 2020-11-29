e5db808540f90d90f89596f280808f7aa52e8e78
Jamie Wilkinson <jaq@spacepants.org>
2020-11-29 17:40:06 +1100

    Add a test for socketstream.
    Learn that we cannot create a socket under test with this API as it's trying to
    stat the object to find out what it is, but sockets must be created by the
    listener, which in this case is mtail.  We'll need to add some magic to parse
    socket names in the log patterns.

---
+++ internal/tailer/logstream/socketstream_test.go
@@ -0,0 +1,50 @@
+// Copyright 2020 Google Inc. All Rights Reserved.
+// This file is available under the Apache license.
+
+package logstream_test
+
+import (
+	"context"
+	"net"
+	"path/filepath"
+	"sync"
+	"testing"
+
+	"github.com/google/mtail/internal/logline"
+	"github.com/google/mtail/internal/tailer/logstream"
+	"github.com/google/mtail/internal/testutil"
+)
+
+func TestSocketStreamPoll(t *testing.T) {
+	t.Skip("logstream.New cannot stat a nonexistent socket")
+	var wg sync.WaitGroup
+
+	tmpDir, rmTmpDir := testutil.TestTempDir(t)
+	defer rmTmpDir()
+
+	name := filepath.Join(tmpDir, "sock")
+
+	sp := NewStubProcessor()
+
+	ctx, cancel := context.WithCancel(context.Background())
+	ss, err := logstream.New(ctx, &wg, name, sp)
+	testutil.FatalIfErr(t, err)
+	ss.Poll() // Synchronise past socket creation
+
+	s, err := net.DialUnix("unixgram", nil, &net.UnixAddr{name, "unixgram"})
+	testutil.FatalIfErr(t, err)
+
+	sp.ExpectLinesReceived(1)
+	_, err = s.Write([]byte("1\n"))
+	testutil.FatalIfErr(t, err)
+	ss.Poll()
+
+	sp.Verify()
+	expected := []logline.LogLine{
+		{context.TODO(), name, "1"},
+	}
+	testutil.ExpectNoDiff(t, expected, sp.Result(), testutil.IgnoreFields(logline.LogLine{}, "Context"))
+
+	cancel()
+	wg.Wait()
+}
