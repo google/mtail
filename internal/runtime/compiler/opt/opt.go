7fc2c33f1c37b7f8f6b36bee5c2a67d3e3c91d44
Jamie Wilkinson <jaq@spacepants.org>
2021-05-09 16:04:57 +1000

    Const fold on all binary math ops.

--- internal/runtime/compiler/opt/opt.go
+++ internal/runtime/compiler/opt/opt.go
@@ -5,6 +5,8 @@
 package opt
 
 import (
+	"math"
+
 	"github.com/google/mtail/internal/runtime/compiler/ast"
 	"github.com/google/mtail/internal/runtime/compiler/parser"
 )
@@ -24,22 +26,99 @@ func (o *optimiser) VisitBefore(node ast.Node) (ast.Visitor, ast.Node) {
 func (o *optimiser) VisitAfter(node ast.Node) ast.Node {
 	switch n := node.(type) {
 	case *ast.BinaryExpr:
-		lhs, ok := n.Lhs.(*ast.IntLit)
-		if !ok {
-			return node
-		}
-		rhs, ok := n.Rhs.(*ast.IntLit)
-		if !ok {
-			return node
-		}
-		var r int64
-		switch n.Op {
-		case parser.PLUS:
-			r = lhs.I + rhs.I
+		switch lhs := n.Lhs.(type) {
+		case *ast.IntLit:
+			switch rhs := n.Rhs.(type) {
+			case *ast.IntLit:
+				switch n.Op {
+				case parser.PLUS:
+					lhs.I += rhs.I
+				case parser.MINUS:
+					lhs.I -= rhs.I
+				case parser.MUL:
+					lhs.I *= rhs.I
+				case parser.DIV:
+					lhs.I /= rhs.I
+				case parser.MOD:
+					lhs.I %= rhs.I
+				case parser.POW:
+					return &ast.FloatLit{
+						F: math.Pow(float64(lhs.I), float64(rhs.I)),
+						P: *ast.MergePosition(&(lhs.P), &(rhs.P)),
+					}
+				default:
+					return node
+				}
+				lhs.P = *ast.MergePosition(&(lhs.P), &(rhs.P))
+				return lhs
+			case *ast.FloatLit:
+				switch n.Op {
+				case parser.PLUS:
+					rhs.F += float64(lhs.I)
+				case parser.MINUS:
+					rhs.F = float64(lhs.I) - rhs.F
+				case parser.MUL:
+					rhs.F *= float64(lhs.I)
+				case parser.DIV:
+					rhs.F = float64(lhs.I) / rhs.F
+				case parser.MOD:
+					rhs.F = math.Mod(float64(lhs.I), rhs.F)
+				case parser.POW:
+					rhs.F = math.Pow(float64(lhs.I), rhs.F)
+				default:
+					return node
+				}
+				rhs.P = *ast.MergePosition(&(lhs.P), &(rhs.P))
+				return rhs
+			default:
+				return node
+			}
+		case *ast.FloatLit:
+			switch rhs := n.Rhs.(type) {
+			case *ast.IntLit:
+				switch n.Op {
+				case parser.PLUS:
+					lhs.F += float64(rhs.I)
+				case parser.MINUS:
+					lhs.F -= float64(rhs.I)
+				case parser.MUL:
+					lhs.F *= float64(rhs.I)
+				case parser.DIV:
+					lhs.F /= float64(rhs.I)
+				case parser.MOD:
+					lhs.F = math.Mod(lhs.F, float64(rhs.I))
+				case parser.POW:
+					lhs.F = math.Pow(lhs.F, float64(rhs.I))
+				default:
+					return node
+				}
+				lhs.P = *ast.MergePosition(&(lhs.P), &(rhs.P))
+				return lhs
+			case *ast.FloatLit:
+				switch n.Op {
+				case parser.PLUS:
+					lhs.F += rhs.F
+				case parser.MINUS:
+					lhs.F -= rhs.F
+				case parser.MUL:
+					lhs.F *= rhs.F
+				case parser.DIV:
+					lhs.F /= rhs.F
+				case parser.MOD:
+					lhs.F = math.Mod(lhs.F, rhs.F)
+				case parser.POW:
+					lhs.F = math.Pow(lhs.F, rhs.F)
+				default:
+					return node
+				}
+				lhs.P = *ast.MergePosition(&(lhs.P), &(rhs.P))
+				return lhs
+			default:
+				return node
+			}
 		default:
 			return node
 		}
-		return &ast.IntLit{I: r}
 	}
 	return node
 }
--- internal/runtime/compiler/opt/opt_test.go
+++ internal/runtime/compiler/opt/opt_test.go
@@ -12,14 +12,56 @@ import (
 	"github.com/google/mtail/internal/testutil"
 )
 
+var optimiserTests = []struct {
+	name string
+	ast  ast.Node
+	want ast.Node
+}{
+	{
+		"int add",
+		&ast.BinaryExpr{
+			Lhs: &ast.IntLit{I: 1},
+			Rhs: &ast.IntLit{I: 2},
+			Op:  parser.PLUS,
+		},
+		&ast.IntLit{I: 3},
+	},
+	{
+		"float mul",
+		&ast.BinaryExpr{
+			Lhs: &ast.FloatLit{F: 2},
+			Rhs: &ast.FloatLit{F: 3},
+			Op:  parser.MUL,
+		},
+		&ast.FloatLit{F: 6},
+	},
+	{
+		"int float pow",
+		&ast.BinaryExpr{
+			Lhs: &ast.IntLit{I: 2},
+			Rhs: &ast.FloatLit{F: 3},
+			Op:  parser.POW,
+		},
+		&ast.FloatLit{F: 8},
+	},
+	{
+		"float int mod",
+		&ast.BinaryExpr{
+			Lhs: &ast.FloatLit{F: 3},
+			Rhs: &ast.IntLit{I: 2},
+			Op:  parser.MOD,
+		},
+		&ast.FloatLit{F: 1},
+	},
+}
+
 func TestOptimiser(t *testing.T) {
-	n := &ast.BinaryExpr{
-		Lhs: &ast.IntLit{I: 1},
-		Rhs: &ast.IntLit{I: 2},
-		Op:  parser.PLUS,
+	for _, tc := range optimiserTests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := opt.Optimise(tc.ast)
+			testutil.FatalIfErr(t, err)
+			testutil.ExpectNoDiff(t, tc.want, got)
+		})
 	}
-	want := &ast.IntLit{I: 3}
-	got, err := opt.Optimise(n)
-	testutil.FatalIfErr(t, err)
-	testutil.ExpectNoDiff(t, want, got)
 }
