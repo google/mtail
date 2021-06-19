b399f8c66aec9ef0760cba5ddbc808e45ee4132c
Jamie Wilkinson <jaq@spacepants.org>
2021-06-19 13:41:05 +1000

    Test socket and datagram streams against filestream.
    Use the TimeoutTest method to run each of these in their own local timeout.

--- internal/mtail/examples_integration_test.go
+++ internal/mtail/examples_integration_test.go
@@ -12,6 +12,7 @@ import (
 	"path/filepath"
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/golang/glog"
 	"github.com/google/mtail/internal/metrics"
@@ -22,6 +23,8 @@ import (
 	"github.com/google/mtail/internal/waker"
 	"golang.org/x/sys/unix"
 )
+
+const exampleTimeout = 2 * time.Second
 
 var exampleProgramTests = []struct {
 	programfile string // Example program file.
@@ -88,38 +91,39 @@ var exampleProgramTests = []struct {
 func TestExamplePrograms(t *testing.T) {
 	testutil.SkipIfShort(t)
 	for _, tc := range exampleProgramTests {
-		t.Run(fmt.Sprintf("%s on %s", tc.programfile, tc.logfile), func(t *testing.T) {
-			ctx, cancel := context.WithCancel(context.Background())
-			waker, _ := waker.NewTest(ctx, 0) // oneshot means we should never need to wake the stream
-			store := metrics.NewStore()
-			programFile := filepath.Join("../..", tc.programfile)
-			mtail, err := mtail.New(ctx, store, mtail.ProgramPath(programFile), mtail.LogPathPatterns(tc.logfile), mtail.OneShot, mtail.OmitMetricSource, mtail.DumpAstTypes, mtail.DumpBytecode, mtail.LogPatternPollWaker(waker), mtail.LogstreamPollWaker(waker))
-			testutil.FatalIfErr(t, err)
+		t.Run(fmt.Sprintf("%s on %s", tc.programfile, tc.logfile),
+			testutil.TimeoutTest(exampleTimeout, func(t *testing.T) {
+				ctx, cancel := context.WithCancel(context.Background())
+				waker, _ := waker.NewTest(ctx, 0) // oneshot means we should never need to wake the stream
+				store := metrics.NewStore()
+				programFile := filepath.Join("../..", tc.programfile)
+				mtail, err := mtail.New(ctx, store, mtail.ProgramPath(programFile), mtail.LogPathPatterns(tc.logfile), mtail.OneShot, mtail.OmitMetricSource, mtail.DumpAstTypes, mtail.DumpBytecode, mtail.LogPatternPollWaker(waker), mtail.LogstreamPollWaker(waker))
+				testutil.FatalIfErr(t, err)
 
-			var wg sync.WaitGroup
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				testutil.FatalIfErr(t, mtail.Run())
-			}()
-			// Oneshot mode means we can wait for shutdown before cancelling.
-			wg.Wait()
-			cancel()
+				var wg sync.WaitGroup
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					testutil.FatalIfErr(t, mtail.Run())
+				}()
+				// Oneshot mode means we can wait for shutdown before cancelling.
+				wg.Wait()
+				cancel()
 
-			g, err := os.Open(tc.goldenfile)
-			testutil.FatalIfErr(t, err)
-			defer g.Close()
+				g, err := os.Open(tc.goldenfile)
+				testutil.FatalIfErr(t, err)
+				defer g.Close()
 
-			goldenStore := golden.ReadTestData(g, tc.programfile)
+				goldenStore := golden.ReadTestData(g, tc.programfile)
 
-			var storeList metrics.MetricSlice
-			store.Range(func(m *metrics.Metric) error {
-				storeList = append(storeList, m)
-				return nil
-			})
+				var storeList metrics.MetricSlice
+				store.Range(func(m *metrics.Metric) error {
+					storeList = append(storeList, m)
+					return nil
+				})
 
-			testutil.ExpectNoDiff(t, goldenStore, storeList, testutil.SortSlices(metrics.MetricsLess), testutil.IgnoreUnexported(metrics.Metric{}, sync.RWMutex{}, datum.String{}))
-		})
+				testutil.ExpectNoDiff(t, goldenStore, storeList, testutil.SortSlices(metrics.MetricsLess), testutil.IgnoreUnexported(metrics.Metric{}, sync.RWMutex{}, datum.String{}))
+			}))
 	}
 }
 
@@ -189,93 +193,35 @@ func TestFilePipeStreamComparison(t *testing.T) {
 
 	for _, tc := range exampleProgramTests {
 		tc := tc
-		t.Run(fmt.Sprintf("%s on %s", tc.programfile, tc.logfile), func(t *testing.T) {
-			ctx, cancel := context.WithCancel(context.Background())
-			waker := waker.NewTestAlways()
-			fileStore, pipeStore := metrics.NewStore(), metrics.NewStore()
-			programFile := filepath.Join("../..", tc.programfile)
-
-			// Set up the pipe
-			tmpDir := testutil.TestTempDir(t)
-
-			pipeName := filepath.Join(tmpDir, filepath.Base(tc.logfile))
-			testutil.FatalIfErr(t, unix.Mkfifo(pipeName, 0600))
-
-			var wg sync.WaitGroup
-			wg.Add(3)
-			go func() {
-				defer wg.Done()
-				source, err := os.OpenFile(tc.logfile, os.O_RDONLY, 0)
-				testutil.FatalIfErr(t, err)
-				// not NONBLOCK to wait for pipeMtail to start reading the pipe
-				pipe, err := os.OpenFile(pipeName, os.O_WRONLY, os.ModeNamedPipe)
-				testutil.FatalIfErr(t, err)
-				n, err := io.Copy(pipe, source)
-				testutil.FatalIfErr(t, err)
-				glog.Infof("Copied %d bytes into pipe", n)
-				source.Close()
-				pipe.Close()
-			}()
-
-			go func() {
-				defer wg.Done()
-				fileMtail, err := mtail.New(ctx, fileStore, mtail.ProgramPath(programFile), mtail.LogPathPatterns(tc.logfile), mtail.OneShot, mtail.OmitMetricSource, mtail.LogPatternPollWaker(waker), mtail.LogstreamPollWaker(waker))
-				if err != nil {
-					t.Error(err)
-				}
-				if err := fileMtail.Run(); err != nil {
-					t.Error(err)
-				}
-			}()
-			pipeMtail, err := mtail.New(ctx, pipeStore, mtail.ProgramPath(programFile), mtail.LogPathPatterns(pipeName), mtail.OneShot, mtail.OmitMetricSource, mtail.LogPatternPollWaker(waker), mtail.LogstreamPollWaker(waker))
-			testutil.FatalIfErr(t, err)
-			go func() {
-				defer wg.Done()
-				if err := pipeMtail.Run(); err != nil {
-					t.Error(err)
-				}
-			}()
-
-			// Oneshot mode means we can wait for shutdown before cancelling.
-			wg.Wait()
-			cancel()
-
-			var pipeMetrics, fileMetrics metrics.MetricSlice
-			pipeStore.Range(func(m *metrics.Metric) error {
-				pipeMetrics = append(pipeMetrics, m)
-				return nil
-			})
-			fileStore.Range(func(m *metrics.Metric) error {
-				fileMetrics = append(fileMetrics, m)
-				return nil
-			})
-
-			// Ignore the datum.Time field as well, as the results will be unstable otherwise.
-			testutil.ExpectNoDiff(t, fileMetrics, pipeMetrics, testutil.SortSlices(metrics.MetricsLess), testutil.IgnoreUnexported(metrics.Metric{}, sync.RWMutex{}, datum.String{}), testutil.IgnoreFields(datum.BaseDatum{}, "Time"))
-		})
-	}
-}
-
-func TestFileSocketStreamComparison(t *testing.T) {
-	testutil.SkipIfShort(t)
-
-	for _, scheme := range []string{"unixgram", "unix"} {
-		scheme := scheme
-		for _, tc := range exampleProgramTests {
-			tc := tc
-			t.Run(fmt.Sprintf("%s on %s://%s", tc.programfile, scheme, tc.logfile), func(t *testing.T) {
+		t.Run(fmt.Sprintf("%s on %s", tc.programfile, tc.logfile),
+			testutil.TimeoutTest(exampleTimeout, func(t *testing.T) {
 				ctx, cancel := context.WithCancel(context.Background())
 				waker := waker.NewTestAlways()
-				fileStore, sockStore := metrics.NewStore(), metrics.NewStore()
+				fileStore, pipeStore := metrics.NewStore(), metrics.NewStore()
 				programFile := filepath.Join("../..", tc.programfile)
 
-				// Set up the socket
+				// Set up the pipe
 				tmpDir := testutil.TestTempDir(t)
 
-				sockName := filepath.Join(tmpDir, filepath.Base(tc.logfile))
+				pipeName := filepath.Join(tmpDir, filepath.Base(tc.logfile))
+				testutil.FatalIfErr(t, unix.Mkfifo(pipeName, 0600))
 
 				var wg sync.WaitGroup
 				wg.Add(3)
+				go func() {
+					defer wg.Done()
+					source, err := os.OpenFile(tc.logfile, os.O_RDONLY, 0)
+					testutil.FatalIfErr(t, err)
+					// not NONBLOCK to wait for pipeMtail to start reading the pipe
+					pipe, err := os.OpenFile(pipeName, os.O_WRONLY, os.ModeNamedPipe)
+					testutil.FatalIfErr(t, err)
+					n, err := io.Copy(pipe, source)
+					testutil.FatalIfErr(t, err)
+					glog.Infof("Copied %d bytes into pipe", n)
+					source.Close()
+					pipe.Close()
+				}()
+
 				go func() {
 					defer wg.Done()
 					fileMtail, err := mtail.New(ctx, fileStore, mtail.ProgramPath(programFile), mtail.LogPathPatterns(tc.logfile), mtail.OneShot, mtail.OmitMetricSource, mtail.LogPatternPollWaker(waker), mtail.LogstreamPollWaker(waker))
@@ -286,35 +232,22 @@ func TestFileSocketStreamComparison(t *testing.T) {
 						t.Error(err)
 					}
 				}()
-				sockMtail, err := mtail.New(ctx, sockStore, mtail.ProgramPath(programFile), mtail.LogPathPatterns(scheme+"://"+sockName), mtail.OneShot, mtail.OmitMetricSource, mtail.LogPatternPollWaker(waker), mtail.LogstreamPollWaker(waker))
+				pipeMtail, err := mtail.New(ctx, pipeStore, mtail.ProgramPath(programFile), mtail.LogPathPatterns(pipeName), mtail.OneShot, mtail.OmitMetricSource, mtail.LogPatternPollWaker(waker), mtail.LogstreamPollWaker(waker))
 				testutil.FatalIfErr(t, err)
 				go func() {
 					defer wg.Done()
-					if err := sockMtail.Run(); err != nil {
+					if err := pipeMtail.Run(); err != nil {
 						t.Error(err)
 					}
-				}()
-
-				go func() {
-					defer wg.Done()
-					source, err := os.OpenFile(tc.logfile, os.O_RDONLY, 0)
-					testutil.FatalIfErr(t, err)
-					s, err := net.DialUnix(scheme, nil, &net.UnixAddr{sockName, scheme})
-					testutil.FatalIfErr(t, err)
-					n, err := io.Copy(s, source)
-					testutil.FatalIfErr(t, err)
-					glog.Infof("Copied %d bytes into socket", n)
-					source.Close()
-					s.Close()
 				}()
 
 				// Oneshot mode means we can wait for shutdown before cancelling.
 				wg.Wait()
 				cancel()
 
-				var sockMetrics, fileMetrics metrics.MetricSlice
-				sockStore.Range(func(m *metrics.Metric) error {
-					sockMetrics = append(sockMetrics, m)
+				var pipeMetrics, fileMetrics metrics.MetricSlice
+				pipeStore.Range(func(m *metrics.Metric) error {
+					pipeMetrics = append(pipeMetrics, m)
 					return nil
 				})
 				fileStore.Range(func(m *metrics.Metric) error {
@@ -323,8 +256,81 @@ func TestFileSocketStreamComparison(t *testing.T) {
 				})
 
 				// Ignore the datum.Time field as well, as the results will be unstable otherwise.
-				testutil.ExpectNoDiff(t, fileMetrics, sockMetrics, testutil.SortSlices(metrics.MetricsLess), testutil.IgnoreUnexported(metrics.Metric{}, sync.RWMutex{}, datum.String{}), testutil.IgnoreFields(datum.BaseDatum{}, "Time"))
-			})
+				testutil.ExpectNoDiff(t, fileMetrics, pipeMetrics, testutil.SortSlices(metrics.MetricsLess), testutil.IgnoreUnexported(metrics.Metric{}, sync.RWMutex{}, datum.String{}), testutil.IgnoreFields(datum.BaseDatum{}, "Time"))
+			}))
+	}
+}
+
+func TestFileSocketStreamComparison(t *testing.T) {
+	testutil.SkipIfShort(t)
+
+	for _, scheme := range []string{"unixgram", "unix"} {
+		scheme := scheme
+		for _, tc := range exampleProgramTests {
+			tc := tc
+			t.Run(fmt.Sprintf("%s on %s://%s", tc.programfile, scheme, tc.logfile),
+				testutil.TimeoutTest(exampleTimeout, func(t *testing.T) {
+					ctx, cancel := context.WithCancel(context.Background())
+					waker := waker.NewTestAlways()
+					fileStore, sockStore := metrics.NewStore(), metrics.NewStore()
+					programFile := filepath.Join("../..", tc.programfile)
+
+					// Set up the socket
+					tmpDir := testutil.TestTempDir(t)
+
+					sockName := filepath.Join(tmpDir, filepath.Base(tc.logfile))
+
+					var wg sync.WaitGroup
+					wg.Add(3)
+					go func() {
+						defer wg.Done()
+						fileMtail, err := mtail.New(ctx, fileStore, mtail.ProgramPath(programFile), mtail.LogPathPatterns(tc.logfile), mtail.OneShot, mtail.OmitMetricSource, mtail.LogPatternPollWaker(waker), mtail.LogstreamPollWaker(waker))
+						if err != nil {
+							t.Error(err)
+						}
+						if err := fileMtail.Run(); err != nil {
+							t.Error(err)
+						}
+					}()
+					sockMtail, err := mtail.New(ctx, sockStore, mtail.ProgramPath(programFile), mtail.LogPathPatterns(scheme+"://"+sockName), mtail.OneShot, mtail.OmitMetricSource, mtail.LogPatternPollWaker(waker), mtail.LogstreamPollWaker(waker))
+					testutil.FatalIfErr(t, err)
+					go func() {
+						defer wg.Done()
+						if err := sockMtail.Run(); err != nil {
+							t.Error(err)
+						}
+					}()
+
+					go func() {
+						defer wg.Done()
+						source, err := os.OpenFile(tc.logfile, os.O_RDONLY, 0)
+						testutil.FatalIfErr(t, err)
+						s, err := net.DialUnix(scheme, nil, &net.UnixAddr{sockName, scheme})
+						testutil.FatalIfErr(t, err)
+						n, err := io.Copy(s, source)
+						testutil.FatalIfErr(t, err)
+						glog.Infof("Copied %d bytes into socket", n)
+						source.Close()
+						s.Close()
+					}()
+
+					// Oneshot mode means we can wait for shutdown before cancelling.
+					wg.Wait()
+					cancel()
+
+					var sockMetrics, fileMetrics metrics.MetricSlice
+					sockStore.Range(func(m *metrics.Metric) error {
+						sockMetrics = append(sockMetrics, m)
+						return nil
+					})
+					fileStore.Range(func(m *metrics.Metric) error {
+						fileMetrics = append(fileMetrics, m)
+						return nil
+					})
+
+					// Ignore the datum.Time field as well, as the results will be unstable otherwise.
+					testutil.ExpectNoDiff(t, fileMetrics, sockMetrics, testutil.SortSlices(metrics.MetricsLess), testutil.IgnoreUnexported(metrics.Metric{}, sync.RWMutex{}, datum.String{}), testutil.IgnoreFields(datum.BaseDatum{}, "Time"))
+				}))
 		}
 	}
 }
