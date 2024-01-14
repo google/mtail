5571ca08ba1417da66ce3c5626274a8a32c86a05
Jamie Wilkinson <jaq@spacepants.org>
2024-01-14 17:33:18 +1100

    Make sure pathname errors return an error.

--- internal/tailer/logstream/logstream_test.go
+++ internal/tailer/logstream/logstream_test.go
@@ -2,6 +2,7 @@ package logstream
 
 import (
 	"context"
+	"sync"
 	"testing"
 )
 
@@ -10,5 +11,10 @@ func TestNewErrors(t *testing.T) {
 	_, err := New(ctx, nil, nil, "", nil, false)
 	if err == nil {
 		t.Errorf("New(ctx, nil) expecting error, received nil")
+	}
+	var wg sync.WaitGroup
+	_, err = New(ctx, &wg, nil, ":a/b", nil, false)
+	if err == nil {
+		t.Error("New(ctg, wg, ..., path) expecting error, received nil")
 	}
 }
